ae4cb0d39d71e5a590449162ee58776af9babc5c
hadley <h.wickham@gmail.com>
2016-02-24 11:49:04 -0600

    Tests for unquote splicing

--- tests/testthat/test-quasiquote.R
+++ tests/testthat/test-quasiquote.R
@@ -16,6 +16,22 @@ test_that("mixes the two", {
   expect_identical(quasiquote_(quote(1 + ((1 + 1)))), quote(1 + 2))
 })
 
+test_that("contents of ({}) must be a list", {
+  expr <- quote(1 + ({2}))
+
+  expect_error(quasiquote_(expr), "must evaluate to a list")
+})
+
+test_that("values of ({}) spliced into expression", {
+  expr <- quote(f(a, ({ list(quote(b), quote(c)) }), d))
+  expect_identical(quasiquote_(expr), quote(f(a, b, c, d)))
+})
+
+test_that("names within  ({}) are preseved expression", {
+  expr <- quote(f(({ list(a = quote(b)) })))
+  expect_identical(quasiquote_(expr), quote(f(a = b)))
+})
+
 # unquote/unquote_splice detection -------------------------------------------------------
 
 test_that("unquote detects paired parens", {
