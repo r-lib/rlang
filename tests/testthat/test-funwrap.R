93ce7139e2a32b5a2ead8e2e5818cf8487934a53
hadley <h.wickham@gmail.com>
2016-02-24 12:01:53 -0600

    Test for funwrap f with empty env

--- tests/testthat/test-funwrap.R
+++ tests/testthat/test-funwrap.R
@@ -12,3 +12,8 @@ test_that("funwrap substitutes even in globalenv", {
   .GlobalEnv$`__1` <- 1
   expect_equal(rhs(funwrap(make_formula(quote(`__1`), globalenv()))), 1)
 })
+
+test_that("doesn't go past empty env", {
+  f <- make_formula(quote(x == y), emptyenv())
+  expect_equal(funwrap(f), f)
+})
