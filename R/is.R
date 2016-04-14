809e0de79e1cd4916a236bc9fe16053824a87b91
hadley <h.wickham@gmail.com>
2016-04-14 10:17:19 -0500

    Use is_lang for more consistent length

--- R/is.R
+++ R/is.R
@@ -8,33 +8,33 @@
 #' @export
 #' @examples
 #' q1 <- quote(1)
-#' is_language(q1)
+#' is_lang(q1)
 #' is_atomic(q1)
 #'
 #' q2 <- quote(x)
-#' is_language(q2)
+#' is_lang(q2)
 #' is_name(q2)
 #'
 #' q3 <- quote(x + 1)
-#' is_language(q3)
+#' is_lang(q3)
 #' is_call(q3)
-is_language <- function(x) {
+is_lang <- function(x) {
   is_name(x) || is_call(x) || is_atomic(x)
 }
 
-#' @rdname is_language
+#' @rdname is_lang
 #' @export
 is_name <- function(x) {
   typeof(x) == "symbol"
 }
 
-#' @rdname is_language
+#' @rdname is_lang
 #' @export
 is_call <- function(x) {
   typeof(x) == "call"
 }
 
-#' @rdname is_language
+#' @rdname is_lang
 #' @export
 is_atomic <- function(x) {
   typeof(x) %in% c("logical", "integer", "double", "complex", "character", "raw")
