5db46212e4847c31f89544a1dd28dab6822ad218
hadley <h.wickham@gmail.com>
2016-04-14 10:10:11 -0500

    Add consistent is wrappers

---
+++ R/is.R
@@ -0,0 +1,42 @@
+#' Is an object a language object?
+#'
+#' These helpers are consistent wrappers around their base R equivalents.
+#' A language object is either an atomic vector (typically a scalar), a
+#' name (aka a symbol), or a call.
+#'
+#' @param x An object to test.
+#' @export
+#' @examples
+#' q1 <- quote(1)
+#' is_language(q1)
+#' is_atomic(q1)
+#'
+#' q2 <- quote(x)
+#' is_language(q2)
+#' is_name(q2)
+#'
+#' q3 <- quote(x + 1)
+#' is_language(q3)
+#' is_call(q3)
+is_language <- function(x) {
+  is_name(x) || is_call(x) || is_atomic(x)
+}
+
+#' @rdname is_language
+#' @export
+is_name <- function(x) {
+  typeof(x) == "symbol"
+}
+
+#' @rdname is_language
+#' @export
+is_call <- function(x) {
+  typeof(x) == "call"
+}
+
+#' @rdname is_language
+#' @export
+is_atomic <- function(x) {
+  typeof(x) %in% c("logical", "integer", "double", "complex", "character", "raw")
+}
+
