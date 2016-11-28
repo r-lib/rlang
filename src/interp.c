aca3596f06c13db11bb6ff5da12b73e9d1b3f541
Lionel Henry <lionel.hry@gmail.com>
2016-11-28 10:29:08 +0100

    Treat bangs as unquoting operator (WIP)

--- src/interp.c
+++ src/interp.c
@@ -3,9 +3,33 @@
 #include <Rdefines.h>
 #include "utils.h"
 
+int is_bang(SEXP x) {
+  if (!is_call_to(x, "!"))
+    return 0;
+
+  SEXP arg = CDR(x);
+  if (TYPEOF(arg) == NILSXP || !is_call_to(CAR(arg), "!"))
+    return 1;
+
+  arg = CDR(CAR(arg));
+  if (TYPEOF(arg) == NILSXP || !is_call_to(CAR(arg), "!"))
+    return 2;
+
+  return 3;
+}
+
 SEXP interp_walk(SEXP x, SEXP env, SEXP data)  {
   if (!Rf_isLanguage(x))
     return x;
+
+  int bang = is_bang(x);
+  if (bang == 3 || is_call_to(x, "uqs"))
+    Rf_error("Cannot splice at top-level");
+
+  if (bang == 2) {
+    x = CADR(x);
+    SETCAR(x, Rf_install("uq"));
+  }
 
   if (is_call_to(x, "uq")) {
     SEXP uq_call = PROTECT(Rf_lang3(Rf_install("uq"), CADR(x), data));
@@ -21,16 +45,23 @@ SEXP interp_walk(SEXP x, SEXP env, SEXP data)  {
   // Recursive case
   for(SEXP cur = x; cur != R_NilValue; cur = CDR(cur)) {
     SETCAR(cur, interp_walk(CAR(cur), env, data));
-
     SEXP nxt = CDR(cur);
+
+    if (is_bang(CAR(nxt)) == 3) {
+      nxt = CDAR(nxt);
+      nxt = CDAR(nxt);
+      SETCAR(CAR(nxt), Rf_install("uqs"));
+      SETCDR(nxt, CDR(CDR(cur)));
+    }
+
     if (is_call_to(CAR(nxt), "uqs")) {
       // uqs() does error checking and returns a pair list
-      SEXP args_pl = Rf_eval(CAR(nxt), env);
+      SEXP args_lsp = Rf_eval(CAR(nxt), env);
 
-      // Insert args_pl into existing pairlist of args
-      SEXP last_arg = findLast(args_pl);
+      // Insert args_lsp into existing pairlist of args
+      SEXP last_arg = findLast(args_lsp);
       SETCDR(last_arg, CDR(nxt));
-      SETCDR(cur, args_pl);
+      SETCDR(cur, args_lsp);
     }
   }
   return x;
