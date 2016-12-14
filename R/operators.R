845b40e36e23dc27adce46b4fc7a8d6d30a936f0
Lionel Henry <lionel.hry@gmail.com>
2016-12-14 12:31:01 +0000

    Add %|% to provide default to missing values

--- R/operators.R
+++ R/operators.R
@@ -11,7 +11,30 @@
 #' @examples
 #' 1 %||% 2
 #' NULL %||% 2
-`%||%` <- function(x, y) if (is_null(x)) y else x
+`%||%` <- function(x, y) {
+  if (is_null(x)) y else x
+}
+
+#' Replace missing values.
+#'
+#' This infix function is similar to \code{\%||\%} but is vectorised
+#' and provides a default value for missing elements. It is faster
+#' than using \code{\link[base]{ifelse}()} and does not perform type
+#' conversions.
+#'
+#' @param x,y \code{y} for elements of \code{x} that are NA;
+#'   otherwise, \code{x}.
+#' @useDynLib rlang replace_na
+#' @export
+#' @name op-na-default
+#' @seealso \link{op-null-default}
+#' @examples
+#' c("a", "b", NA, "c") %|% "default"
+`%|%` <- function(x, y) {
+  stopifnot(is_atomic(x) && is_scalar_atomic(y))
+  stopifnot(typeof(x) == typeof(y))
+  .Call(replace_na, x, y)
+}
 
 #' Infix attribute accessor
 #'
@@ -22,4 +45,6 @@
 #' @examples
 #' factor(1:3) %@% "levels"
 #' mtcars %@% "class"
-`%@%` <- function(x, name) attr(x, name, exact = TRUE)
+`%@%` <- function(x, name) {
+  attr(x, name, exact = TRUE)
+}
---
+++ src/replace-na.c
@@ -0,0 +1,129 @@
+#define R_NO_REMAP
+#include <Rinternals.h>
+
+SEXP replace_na_(SEXP x, SEXP replacement, int start);
+
+SEXP replace_na(SEXP x, SEXP replacement) {
+  int n = Rf_length(x);
+  int i = 0;
+
+  switch(TYPEOF(x)) {
+  case LGLSXP: {
+    int* arr = LOGICAL(x);
+    for (; i < n; ++i) {
+      if (arr[i] == NA_LOGICAL)
+        break;
+    }
+    break;
+  }
+
+  case INTSXP: {
+    int* arr = INTEGER(x);
+    for (; i < n; ++i) {
+      if (arr[i] == NA_INTEGER)
+        break;
+    }
+    break;
+  }
+
+  case REALSXP: {
+    double* arr = REAL(x);
+    for (; i < n; ++i) {
+      if (ISNA(arr[i]))
+        break;
+    }
+    break;
+  }
+
+  case STRSXP: {
+    for (; i < n; ++i) {
+      if (STRING_ELT(x, i) == NA_STRING)
+        break;
+    }
+    break;
+  }
+
+  case CPLXSXP: {
+    Rcomplex* arr = COMPLEX(x);
+
+    for (; i < n; ++i) {
+      if (ISNA(arr[i].r))
+        break;
+    }
+    break;
+  }
+
+  default: {
+    Rf_error("Don't know how to handle object of type", Rf_type2char(TYPEOF(x)));
+  }
+  }
+
+  if (i < n)
+    return replace_na_(x, replacement, i);
+  else
+    return x;
+}
+
+SEXP replace_na_(SEXP x, SEXP replacement, int i) {
+  PROTECT(x = Rf_duplicate(x));
+  int n = Rf_length(x);
+
+  switch(TYPEOF(x)) {
+  case LGLSXP: {
+    int* arr = LOGICAL(x);
+    int new_value = LOGICAL(replacement)[0];
+    for (; i < n; ++i) {
+      if (arr[i] == NA_LOGICAL)
+        arr[i] = new_value;
+    }
+    break;
+  }
+
+  case INTSXP: {
+    int* arr = INTEGER(x);
+    int new_value = INTEGER(replacement)[0];
+    for (; i < n; ++i) {
+      if (arr[i] == NA_INTEGER)
+        arr[i] = new_value;
+    }
+    break;
+  }
+
+  case REALSXP: {
+    double* arr = REAL(x);
+    double new_value = REAL(replacement)[0];
+    for (; i < n; ++i) {
+      if (ISNA(arr[i]))
+        arr[i] = new_value;
+    }
+    break;
+  }
+
+  case STRSXP: {
+    SEXP new_value = STRING_ELT(replacement, 0);
+    for (; i < n; ++i) {
+      if (STRING_ELT(x, i) == NA_STRING)
+        SET_STRING_ELT(x, i, new_value);
+    }
+    break;
+  }
+
+  case CPLXSXP: {
+    Rcomplex* arr = COMPLEX(x);
+    Rcomplex new_value = COMPLEX(replacement)[0];
+
+    for (; i < n; ++i) {
+      if (ISNA(arr[i].r))
+        arr[i] = new_value;
+    }
+    break;
+  }
+
+  default: {
+    Rf_error("Don't know how to handle object of type", Rf_type2char(TYPEOF(x)));
+  }
+  }
+
+  UNPROTECT(1);
+  return x;
+}
