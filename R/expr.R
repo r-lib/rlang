256b3ac3e090b4d3295ef60acbed035c21a00994
Lionel Henry <lionel.hry@gmail.com>
2017-02-02 00:02:48 +0100

    Implement expr_eval()
    Lightweight wrapper around Rf_eval()

--- R/expr.R
+++ R/expr.R
@@ -48,6 +48,73 @@ expr_quote <- function(expr) {
   .Call(interp_, expr, parent.frame(), FALSE)
 }
 
+#' Evaluate an expression in an environment.
+#'
+#' \code{expr_eval()} is a lightweight version of the base function
+#' \code{\link[base]{eval}()}. It does not accept supplementary data,
+#' but it is more efficient and does not clutter the evaluation stack.
+#' Technically, \code{expr_eval()} is a simple wrapper around the C
+#' function \code{Rf_eval()}.
+#'
+#' \code{base::eval()} inserts two call frames in the stack, the
+#' second of which has the supplied environment as frame
+#' environment. This may unnecessarily clutter the evaluation stack
+#' and it can change base R semantics when evaluating stack sensitive
+#' functions like \code{return()} or \code{parent.frame()}. This is
+#' because these functions search through the stack for the first
+#' occurrence of the environment in which they are evaluated (it is
+#' not necessarily the parent frame because of lazy evaluation
+#' semantics, see \code{\link{eval_stack}()} and
+#' \code{\link{call_stack}()} for more on that distinction). One
+#' consequence is that code evaluated with \code{base::eval()} does
+#' not have the property of stack consistency, and stack sensitive
+#' functions may return misleading results.
+#'
+#' @param expr An expression to evaluate.
+#' @param env The environment in which to evaluate the expression.
+#' @useDynLib rlang rlang_eval
+#' @seealso with_env
+#' @export
+#' @examples
+#' # expr_eval() works just like base::eval():
+#' env <- env_new(dict = list(foo = "bar"))
+#' expr <- quote(foo)
+#' expr_eval(expr, env)
+#'
+#' # To explore the consequences of stack inconsistent semantics, let's
+#' # create a function that evaluates `parent.frame()` deep in the call
+#' # stack, in an environment corresponding to a frame in the middle of
+#' # the stack. For consistency we R's lazy evaluation semantics, we'd
+#' # expect to get the caller of that frame as result:
+#' fn <- function(eval_fn) {
+#'   list(
+#'     returned_env = middle(eval_fn),
+#'     actual_env = env()
+#'   )
+#' }
+#' middle <- function(eval_fn) {
+#'   deep(eval_fn, env())
+#' }
+#' deep <- function(eval_fn, eval_env) {
+#'   expr <- quote(parent.frame())
+#'   eval_fn(expr, eval_env)
+#' }
+#'
+#' # With expr_eval(), we do get the expected environment:
+#' fn(rlang::expr_eval)
+#'
+#' # But that's not the case with base::eval():
+#' fn(base::eval)
+#'
+#' # Another difference of expr_eval() compared to base::eval() is
+#' # that it does not insert parasite frames in the evaluation stack:
+#' get_stack <- quote(identity(eval_stack()))
+#' expr_eval(get_stack)
+#' eval(get_stack)
+expr_eval <- function(expr, env = parent.frame()) {
+  .Call(rlang_eval, expr, env)
+}
+
 
 #' Turn an expression to a label.
 #'
