f5c4d6ca08118de31468c06730f0ffb812213a12
Lionel Henry <lionel.hry@gmail.com>
2017-02-03 15:06:01 +0100

    Move tidy_dots() tests to new test file

--- tests/testthat/test-tidy-capture.R
+++ tests/testthat/test-tidy-capture.R
@@ -1,5 +1,25 @@
 context("tidy capture")
 
+test_that("explicit dots make a list of formulas", {
+  fs <- tidy_dots(x = 1 + 2, y = 2 + 3)
+  f1 <- ~ 1 + 2
+  f2 <- ~ 2 + 3
+
+  expect_identical(fs$x, f1)
+  expect_identical(fs$y, f2)
+})
+
+test_that("tidy_dots() produces correct formulas", {
+  fn <- function(x = a + b, ...) {
+    list(dots = tidy_dots(x = x, y = a + b, ...), env = environment())
+  }
+  out <- fn(z = a + b)
+
+  expect_identical(out$dots$x, with_env(out$env, ~x))
+  expect_identical(out$dots$y, with_env(out$env, ~a + b))
+  expect_identical(out$dots$z, ~a + b)
+})
+
 test_that("dots are interpolated", {
   fn <- function(...) {
     baz <- "baz"
