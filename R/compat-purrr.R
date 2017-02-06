89fc40cc653e7ebfe4ab3d992bf0282288fa4a9f
Lionel Henry <lionel.hry@gmail.com>
2017-02-06 20:09:07 +0100

    Complete compat purrr family

--- R/compat-purrr.R
+++ R/compat-purrr.R
@@ -1,7 +1,76 @@
 
 # This file serves as a reference for compatibility functions for
 # purrr. They are not drop-in replacements but allow a similar style
-# of programming.
+# of programming. This is useful in cases where purrr is too heavy a
+# package to depend on.
+
+vapply_ <- function(.x, .f, .mold, ...) {
+  out <- vapply(.x, .f, .mold, ..., USE.NAMES = FALSE)
+  set_names(out, names(.x))
+}
+vapply_lgl <- function(.x, .f, ...) {
+  vapply_(.x, .f, logical(1), ...)
+}
+vapply_int <- function(.x, .f, ...) {
+  vapply_(.x, .f, integer(1), ...)
+}
+vapply_dbl <- function(.x, .f, ...) {
+  vapply_(.x, .f, double(1), ...)
+}
+vapply_chr <- function(.x, .f, ...) {
+  vapply_(.x, .f, character(1), ...)
+}
+vapply_cpl <- function(.x, .f, ...) {
+  vapply_(.x, .f, complex(1), ...)
+}
+
+pluck <- function(.x, .f) {
+  lapply(.x, `[[`, .f)
+}
+pluck_lgl <- function(.x, .f) {
+  vapply_lgl(.x, `[[`, .f)
+}
+pluck_int <- function(.x, .f) {
+  vapply_int(.x, `[[`, .f)
+}
+pluck_dbl <- function(.x, .f) {
+  vapply_dbl(.x, `[[`, .f)
+}
+pluck_chr <- function(.x, .f) {
+  vapply_chr(.x, `[[`, .f)
+}
+pluck_cpl <- function(.x, .f) {
+  vapply_cpl(.x, `[[`, .f)
+}
+
+probe <- function(.x, .p, ...) {
+  if (is_logical(.p)) {
+    stopifnot(length(.p) == length(.x))
+    .p
+  } else {
+    vapply_lgl(.x, .p, ...)
+  }
+}
+
+keep <- function(.x, .f, ...) {
+  .x[probe(.x, .f, ...)]
+}
+discard <- function(.x, .p, ...) {
+  sel <- probe(.x, .p, ...)
+  .x[is.na(sel) | !sel]
+}
+lapply_if <- function(.x, .p, .f, ...) {
+  matches <- probe(.x, .p)
+  .x[matches] <- lapply(.x[matches], .f, ...)
+  .x
+}
+
+flatten <- function(.x) {
+  unlist(.x, FALSE, FALSE)
+}
+compact <- function(.x) {
+  Filter(length, x)
+}
 
 zip <- function(.l) {
   inner_names <- names(.l[[1]])
@@ -14,26 +83,4 @@ zip <- function(.l) {
   lapply(fields, function(i) {
     lapply(.l, .subset2, i)
   })
-}
-
-vapply_ <- function(.x, .f, .mold, ...) {
-  out <- vapply(.x, .f, .mold, ..., USE.NAMES = FALSE)
-  set_names(out, names(.x))
-}
-vapply_int <- function(.x, .f, ...) {
-  vapply_(.x, .f, integer(1), ...)
-}
-vapply_lgl <- function(.x, .f, ...) {
-  vapply_(.x, .f, logical(1), ...)
-}
-
-pluck <- function(.x, .f) {
-  lapply(.x, `[[`, .f)
-}
-pluck_int <- function(.x, .f) {
-  vapply_int(.x, `[[`, .f)
-}
-
-keep <- function(.x, .f, ...) {
-  .x[vapply_lgl(.x, .f, ...)]
 }
