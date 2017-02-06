bf84cb2c574c7e8e6dd7bc7b2dc229c656d48f4b
Lionel Henry <lionel.hry@gmail.com>
2017-02-06 20:30:00 +0100

    Add compat-oldrel.R
    Packages should copy (parts of) that file to get compatibility with
    old R versions.

---
+++ R/compat-oldrel.R
@@ -0,0 +1,21 @@
+
+# This file serves as a reference for compatibility functions for old
+# versions of R.
+
+
+# R 3.2.0 ------------------------------------------------------------
+
+if (utils::packageVersion("base") < "3.2.0") {
+
+  dir_exists <- function(path) {
+    !identical(path, "") && file.exists(paste0(path, .Platform$file.sep))
+  }
+  dir.exists <- function(paths) {
+    vapply_lgl(paths, dir_exists)
+  }
+
+  names.environment <- function(x) {
+    ls(x, all.names = TRUE, sorted = FALSE)
+  }
+
+}
