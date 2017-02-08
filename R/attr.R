1ef5ff85ded396bcc176dbfada58622bb54f96de
Lionel Henry <lionel.hry@gmail.com>
2017-02-08 23:32:42 +0100

    Add has_name()

--- R/attr.R
+++ R/attr.R
@@ -86,6 +86,25 @@ has_names <- function(x) {
   }
 }
 
+#' Does an object have an element with this name?
+#'
+#' This function returns a logical value that indicates if a data frame or
+#' another named object contains an element with a specific name.
+#'
+#' Unnamed objects are treated as if all names are empty strings.
+#' `NA` input gives `FALSE` as output.
+#'
+#' @param x A data frame or another named object
+#' @param name Element name(s) to check
+#' @return A logical vector of the same length as `name`
+#' @examples
+#' has_name(iris, "Species")
+#' has_name(mtcars, "gears")
+#' @export
+has_name <- function(x, name) {
+  name %in% names2(x)
+}
+
 #' Set names of a vector.
 #'
 #' This is a snake case wrapper for \code{\link[stats]{setNames}},
