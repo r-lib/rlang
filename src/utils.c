d1b79bb9688fb3586ef72fd1750d180418b008bc
Lionel Henry <lionel.hry@gmail.com>
2017-02-16 19:26:35 +0100

    Add symbol constructors
    Which handle encodings correctly.

--- src/utils.c
+++ src/utils.c
@@ -153,3 +153,8 @@ SEXP rlang_fun(SEXP sym) {
   UNPROTECT(1);
   return fun;
 }
+
+SEXP rlang_symbol(SEXP chr) {
+  SEXP string = STRING_ELT(chr, 0);
+  return Rf_install(Rf_translateChar(string));
+}
