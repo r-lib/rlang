83567e9dc7edb248cf6a82f1fd162565f68ec338
Lionel Henry <lionel.hry@gmail.com>
2017-02-23 19:23:20 +0100

    Revert "Never reencode strings when converting to symbols"
    This reverts commit 5156d92b645c02755a4751df58cffb9148a0a2c6.

--- R/types-sym.R
+++ R/types-sym.R
@@ -1,10 +1,15 @@
 #' Create a symbol or list of symbols.
 #'
 #' These functions take strings as input and turn them into symbols.
+#' Contrarily to \code{as.name()}, they convert the strings to the
+#' native encoding beforehand. This is necessary because symbols
+#' remove silently the encoding mark of strings (see
+#' \code{\link{str_set_encoding}()}).
 #'
 #' @param x A string or list of strings.
 #' @return A symbol for \code{symbol()} and a list of symbols for
 #'   \code{symbols()}.
+#' @useDynLib rlang rlang_symbol
 #' @export
 symbol <- function(x) {
   if (is_symbol(x)) {
@@ -13,7 +18,7 @@ symbol <- function(x) {
   if (!is_string(x)) {
     abort("Only strings can be converted to symbols")
   }
-  as.name(x)
+  .Call(rlang_symbol, x)
 }
 #' @rdname symbol
 #' @export
