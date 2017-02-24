cee9a1ce1f3749d72959b32bc5dd5d70c0575797
Lionel Henry <lionel.hry@gmail.com>
2017-02-24 20:25:07 +0100

    Add lazyeval compat file for reference

--- tests/testthat/test-compat.R
+++ tests/testthat/test-compat.R
@@ -4,3 +4,58 @@ test_that("names() dispatches on environment", {
   env <- child_env(data = list(foo = "foo", bar = "bar"))
   expect_identical(sort(names(env)), c("bar", "foo"))
 })
+
+test_that("lazy objects are converted to tidy quotes", {
+  env <- child_env(env())
+
+  lazy <- structure(list(expr = quote(foo(bar)), env = env), class = "lazy")
+  expect_warning(regex = "underscored versions are deprecated",
+    expect_identical(compat_lazy(lazy), with_env(env, ~foo(bar)))
+  )
+
+  lazy_str <- "foo(bar)"
+  expect_warning(regex = "Text parsing is deprecated",
+    expect_identical(compat_lazy(lazy_str), ~foo(bar))
+  )
+
+  lazy_lang <- quote(foo(bar))
+  expect_warning(regex = "underscored versions are deprecated",
+    expect_identical(compat_lazy(lazy_lang), ~foo(bar))
+  )
+})
+
+test_that("lazy_dots objects are converted to tidy quotes", {
+  env <- child_env(env())
+
+  lazy_dots <- structure(class = "lazy_dots", list(
+    lazy = structure(list(expr = quote(foo(bar)), env = env), class = "lazy"),
+    lazy_lang = quote(foo(bar))
+  ))
+
+  expected <- list(
+    lazy = with_env(env, ~foo(bar)),
+    lazy_lang = ~foo(bar),
+    ~foo(bar)
+  )
+
+  expect_warning(regex = "underscored versions are deprecated",
+    expect_identical(compat_lazy_dots(lazy_dots, env(), "foo(bar)", .named = FALSE), expected)
+  )
+})
+
+test_that("unnamed lazy_dots are given default names", {
+  lazy_dots <- structure(class = "lazy_dots", list(
+    "foo(baz)",
+    quote(foo(bar))
+  ))
+
+  expected <- list(
+    `foo(baz)` = ~foo(baz),
+    `foo(bar)` = ~foo(bar),
+    foobarbaz = ~foo(barbaz)
+  )
+
+  expect_warning(regex = "underscored versions are deprecated",
+    expect_identical(compat_lazy_dots(lazy_dots, env(), foobarbaz = "foo(barbaz)"), expected)
+  )
+})
