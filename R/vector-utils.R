9bb9b4025c74731118a386b2ccdaa370c216c7e8
Lionel Henry <lionel.hry@gmail.com>
2017-03-08 09:42:45 +0000

    Move vector utils to own file

---
+++ R/vector-utils.R
@@ -0,0 +1,59 @@
+#' Prepend a vector.
+#'
+#' This is a companion to [base::append()] to help merging two lists
+#' or atomic vectors. `prepend()` is a clearer semantic signal than
+#' `c()` that a vector is to be merged at the beginning of another,
+#' especially in a pipe chain.
+#'
+#' @param x the vector to be modified.
+#' @param values to be included in the modified vector.
+#' @param before a subscript, before which the values are to be appended.
+#' @return A merged vector.
+#' @export
+#' @examples
+#' x <- as.list(1:3)
+#'
+#' append(x, "a")
+#' prepend(x, "a")
+#' prepend(x, list("a", "b"), before = 3)
+prepend <- function(x, values, before = 1) {
+  n <- length(x)
+  stopifnot(before > 0 && before <= n)
+
+  if (before == 1) {
+    c(values, x)
+  } else {
+    c(x[1:(before - 1)], values, x[before:n])
+  }
+}
+
+#' Modify a vector.
+#'
+#' This function merges a list of arguments into a vector. It always
+#' returns a list.
+#'
+#' @param .x A vector to modify.
+#' @param ...,.elts List of elements to merge into `.x`. Named
+#'   elements already existing in `.x` are used as replacements.
+#'   Elements that have new or no names are inserted at the end.
+#' @return A modified vector upcasted to a list.
+#' @export
+#' @examples
+#' modify(c(1, b = 2, 3), 4, b = "foo")
+#'
+#' x <- list(a = 1, b = 2)
+#' y <- list(b = 3, c = 4)
+#' modify(x, .elts = y)
+modify <- function(.x, ..., .elts = list()) {
+  out <- as.list(.x)
+  args <- c(list(...), .elts)
+
+  args_nms <- names(args)
+  exists <- have_names(args) & args_nms %in% names(out)
+
+  for (nm in args_nms[exists]) {
+    out[[nm]] <- args[[nm]]
+  }
+
+  c(out, args[!exists])
+}
