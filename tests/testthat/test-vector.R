b97a792dd02dd4ea11059da1f30fa3517405ceee
Lionel Henry <lionel.hry@gmail.com>
2017-03-09 11:21:21 +0000

    Add basic splicing tests

--- tests/testthat/test-vector.R
+++ tests/testthat/test-vector.R
@@ -20,3 +20,41 @@ test_that("are_na() requires vector input but not is_na()", {
   expect_error(are_na(base::eval), "must be a vector")
   expect_false(is_na(base::eval))
 })
+
+test_that("atomic vectors are spliced", {
+  lgl <- lgl(TRUE, c(TRUE, FALSE), list(FALSE, FALSE))
+  expect_identical(lgl, c(TRUE, TRUE, FALSE, FALSE, FALSE))
+
+  int <- int(1L, c(2L, 3L), list(4L, 5L))
+  expect_identical(int, 1:5)
+
+  dbl <- dbl(1, c(2, 3), list(4, 5))
+  expect_identical(dbl, as_double(1:5))
+
+  cpl <- cpl(1i, c(2i, 3i), list(4i, 5i))
+  expect_identical(cpl, c(1i, 2i, 3i, 4i, 5i))
+
+  chr <- chr("foo", c("foo", "bar"), list("buz", "baz"))
+  expect_identical(chr, c("foo", "foo", "bar", "buz", "baz"))
+
+  raw <- bytes(1, c(2, 3), list(4, 5))
+  expect_identical(raw, bytes(1:5))
+})
+
+test_that("can create empty vectors", {
+  expect_identical(lgl(), logical(0))
+  expect_identical(int(), integer(0))
+  expect_identical(dbl(), double(0))
+  expect_identical(cpl(), complex(0))
+  expect_identical(chr(), character(0))
+  expect_identical(bytes(), raw(0))
+  expect_identical(mk_list(), list())
+})
+
+test_that("objects are not spliced", {
+  expect_error(lgl(structure(list(TRUE, TRUE), class = "bam")), "Objects cannot be spliced")
+})
+
+test_that("explicitly spliced lists are spliced", {
+  expect_identical(lgl(FALSE, structure(list(TRUE, TRUE), class = "spliced")), c(FALSE, TRUE, TRUE))
+})
