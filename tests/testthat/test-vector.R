c20773ae7fec762dded68797c24f6a952b88203b
Lionel Henry <lionel.hry@gmail.com>
2017-03-09 16:27:53 +0000

    Remove old splice() function

--- tests/testthat/test-vector.R
+++ tests/testthat/test-vector.R
@@ -1,15 +1,5 @@
 context("vector")
 
-test_that("splice() produces correctly named lists", {
-  inputs <- list(arg1 = "a", arg2 = "b")
-
-  out1 <- splice(inputs, arg3 = c("c1", "c2"))
-  out2 <- splice(inputs, arg = list(arg3 = 1, arg4 = 2))
-
-  expect_equal(names(out1), c("arg1", "arg2", "arg3"))
-  expect_equal(names(out2), c("arg1", "arg2", "arg3", "arg4"))
-})
-
 test_that("vector is modified", {
   x <- c(1, b = 2, c = 3, 4)
   out <- modify(x, 5, b = 20, .elts = list(6, c = "30"))
