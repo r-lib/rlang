18c143e5556f011a05153d12a85eccf5d9ce5728
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-20 12:10:35 +0100

    Add tests

--- tests/testthat/test-encoding.R
+++ tests/testthat/test-encoding.R
@@ -1,5 +1,27 @@
 context("encoding")
 
 test_that("can roundtrip symbols in non-UTF8 locale", {
-  with_non_utf8_encoding(as_character(symbol(get_alien_lang_string())))
+  with_non_utf8_encoding({
+    expect_identical(
+      as_character(symbol(get_alien_lang_string())),
+      get_alien_lang_string()
+    )
+  })
+})
+
+test_that("Unicode escapes are always converted to UTF8 characters on roundtrip", {
+  expect_identical(
+    as_character(symbol("<U+5E78><U+798F>")),
+    "\u5E78\u798F"
+  )
+})
+
+test_that("Unicode escapes are always converted to UTF8 characters on roundtrip", {
+  with_non_utf8_encoding({
+    env <- child_env(empty_env())
+    env_assign(env, get_alien_lang_string(), NULL)
+    expect_identical(names2(env), get_alien_lang_string())
+    list <- as_list(env)
+    expect_identical(names2(list), get_alien_lang_string())
+  })
 })
