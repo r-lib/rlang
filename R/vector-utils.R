0ed962496fd06bd1ea6c53d1faf3376f469c85cc
Lionel Henry <lionel.hry@gmail.com>
2017-04-11 20:01:34 +0200

    Implement seq2() and seq2_along()
    Closes #76

--- R/vector-utils.R
+++ R/vector-utils.R
@@ -58,3 +58,36 @@ modify <- function(.x, ...) {
 
   c(out, args[!exists])
 }
+
+#' Increasing sequence of integers in an interval
+#'
+#' These helpers take two endpoints and return the sequence of all
+#' integers within that interval. For `seq2_along()`, the upper
+#' endpoint is taken from the length of a vector. Unlike
+#' `base::seq()`, they return an empty vector if the starting point is
+#' a larger integer than the end point.
+#'
+#' @param from The starting point of the sequence.
+#' @param to The end point.
+#' @param x A vector whose length is the end point.
+#' @return An integer vector containing a strictly increasing
+#'   sequence.
+#' @export
+#' @examples
+#' seq2(2, 10)
+#' seq2(10, 2)
+#' seq(10, 2)
+#'
+#' seq2_along(10, letters)
+seq2 <- function(from, to) {
+  if (from > to) {
+    int()
+  } else {
+    seq.int(from, to)
+  }
+}
+#' @rdname seq2
+#' @export
+seq2_along <- function(from, x) {
+  seq2(from, length(x))
+}
