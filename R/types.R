1d52a9fedff61e28e271419ede474d36706f816b
Lionel Henry <lionel.hry@gmail.com>
2017-04-18 16:52:41 +0200

    Improve type_of() documentation

--- R/types.R
+++ R/types.R
@@ -329,15 +329,24 @@ is_scalar_integerish <- function(x) {
 #'
 #' # Compare to base::typeof():
 #' typeof(quo(10L))
+#'
+#' # Strings are treated as a new base type:
 #' type_of(letters)
 #' type_of(letters[[1]])
 #'
+#' # This is a bit inconsistent with the core language tenet that data
+#' # types are vectors. However, treating strings as a different
+#' # scalar type is quite helpful for switching on function inputs
+#' # since so many arguments expect strings:
+#' switch_type("foo", character = abort("vector!"), string = "result")
+#'
+#' # Special and builtin primitives are both treated as primitives.
+#' # That's because it is often irrelevant which type of primitive an
+#' # input is:
 #' typeof(list)
 #' typeof(`$`)
 #' type_of(list)
 #' type_of(`$`)
-#'
-#' type_of(quote(base::list()))
 type_of <- function(x) {
   type <- typeof(x)
   if (is_quosure(x)) {
