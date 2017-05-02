809afc7a7b33872d8930144c8e4316e3b14cd8dc
Lionel Henry <lionel.hry@gmail.com>
2017-05-02 18:20:26 +0200

    Test for ... arguments passed to invoke()

--- tests/testthat/test-eval.R
+++ tests/testthat/test-eval.R
@@ -1,13 +1,13 @@
 context("invoke")
 
 test_that("invoke() buries arguments", {
-  expect_identical(invoke(call_inspect, 1:2), quote(.fn(`1`, `2`)))
-  expect_identical(invoke("call_inspect", 1:2), quote(call_inspect(`1`, `2`)))
-  expect_identical(invoke(call_inspect, 1:2, .bury = c("foo", "bar")), quote(foo(`bar1`, `bar2`)))
-  expect_identical(invoke(call_inspect, 1:2, .bury = NULL), as.call(list(call_inspect, 1L, 2L)))
+  expect_identical(invoke(call_inspect, 1:2, 3L), quote(.fn(`1`, `2`, `3`)))
+  expect_identical(invoke("call_inspect", 1:2, 3L), quote(call_inspect(`1`, `2`, `3`)))
+  expect_identical(invoke(call_inspect, 1:2, 3L, .bury = c("foo", "bar")), quote(foo(`bar1`, `bar2`, `bar3`)))
+  expect_identical(invoke(call_inspect, 1:2, 3L, .bury = NULL), as.call(list(call_inspect, 1L, 2L, 3L)))
 })
 
-test_that("invoke() is calls without arguments", {
+test_that("invoke() can be called without arguments", {
   expect_identical(invoke("list"), list())
   expect_identical(invoke(list), list())
 })
