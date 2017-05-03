f611bf1044d3e2d67dae4cd50efba2c7e7925017
Lionel Henry <lionel.hry@gmail.com>
2017-05-03 10:29:40 +0200

    Explain names vs symbols and encoding issues in env_names()

--- R/env.R
+++ R/env.R
@@ -670,11 +670,35 @@ env_get <- function(env = caller_env(), nm, inherit = FALSE) {
   get(nm, envir = get_env(env), inherits = inherit)
 }
 
-#' List object names in an environment.
+#' Names of symbols bound in an environment
 #'
-#' `env_names()` returns object names from an enviroment `env` as a character
-#' vector.
-#' All names are returned, even those starting with a dot.
+#' `env_names()` returns object names from an enviroment `env` as a
+#' character vector. All names are returned, even those starting with
+#' a dot.
+#'
+#' @section Names of symbols and objects:
+#'
+#' Technically, objects are bound to symbols rather than strings,
+#' since the R interpreter evaluates symbols (see [is_expr()] for a
+#' discussion of symbolic objects versus literal objects). However it
+#' is often more convenient to work with strings. In rlang
+#' terminology, the string corresponding to a symbol is called the
+#' _name_ of the symbol (or by extension the name of an object bound
+#' to a symbol).
+#'
+#' @section Encoding:
+#'
+#' There are deep encoding issues when you convert a string to symbol
+#' and vice versa. Symbols are _always_ in the native encoding (see
+#' [set_chr_encoding()]). If that encoding (let's say latin1) cannot
+#' support some characters, these characters are serialised to
+#' ASCII. That's why you sometimes see strings looking like
+#' `<U+1234>`, especially if you're running Windows (as R doesn't
+#' support UTF-8 as native encoding on that platform).
+#'
+#' To alleviate some of the encoding pain, `env_names()` always
+#' returns a UTF-8 character vector (which is fine even on Windows)
+#' with unicode points unserialised.
 #'
 #' @inheritParams get_env
 #' @return A character vector of object names.
