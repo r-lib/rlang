f7a5109caca8bd11676377550989a1c605042d62
Lionel Henry <lionel.hry@gmail.com>
2017-05-05 13:19:16 +0200

    Fix set_names(x, NULL)
    And pass names to chr() to gain splicing semantics

--- R/attr.R
+++ R/attr.R
@@ -175,11 +175,12 @@ has_name <- function(x, name) {
 #'
 #'   * If you do nothing, `x` will be named with itself.
 #'
-#'   * You can supply either a character vector to `nm` or individual
-#'     strings in to `...``
-#'
 #'   * If `x` already has names, you can provide a function or formula
 #'     to transform the existing names.
+#'
+#'   * In all other cases, `nm` and `...` are passed to [chr()]. This
+#'     gives implicit splicing semantics: you can pass character
+#'     vectors or list of character vectors indistinctly.
 #' @export
 #' @examples
 #' set_names(1:4, c("a", "b", "c", "d"))
@@ -192,25 +193,22 @@ has_name <- function(x, name) {
 #' # Alternatively you can supply a function
 #' set_names(1:10, ~ letters[seq_along(.)])
 #' set_names(head(mtcars), toupper)
-set_names <- function(x, nm = NULL, ...) {
+set_names <- function(x, nm = x, ...) {
   if (!is_vector(x)) {
-    stop("`x` must be a vector", call. = FALSE)
+    abort("`x` must be a vector")
   }
 
   if (is_function(nm) || is_formula(nm)) {
     nm <- as_function(nm)
     nm <- nm(names2(x))
-    nm
-  } else {
-    if (is_null(nm) && dots_n(...) == 0) {
-      nm <- as.character(x)
-    } else {
-      nm <- c(nm, ...)
-    }
+  } else if (!is_null(nm)) {
+    # Make sure `x` is serialised when no arguments is provided.
+    nm <- as.character(nm)
+    nm <- chr(nm, ...)
   }
 
-  if (!is.character(nm) || length(x) != length(nm)) {
-    abort("`nm` must be a character vector same length as `x`")
+  if (!is_null(nm) && !is_character(nm, length(x))) {
+    abort("`nm` must be `NULL` or a character vector the same length as `x`")
   }
 
   names(x) <- nm
--- tests/testthat/test-attr.R
+++ tests/testthat/test-attr.R
@@ -36,10 +36,15 @@ test_that("inputs must be valid", {
 test_that("can supply vector or ...", {
   expect_named(set_names(1:2, c("a", "b")), c("a", "b"))
   expect_named(set_names(1:2, "a", "b"), c("a", "b"))
+  expect_named(set_names(1:2, list("a"), list("b")), c("a", "b"))
 })
 
 test_that("can supply function/formula to rename", {
   x <- c(a = 1, b = 2)
   expect_named(set_names(x, toupper), c("A", "B"))
   expect_named(set_names(x, ~ toupper(.)), c("A", "B"))
+})
+
+test_that("set_names() zaps names", {
+  expect_null(names(set_names(mtcars, NULL)))
 })
