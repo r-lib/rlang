304dada2a0cb96977239a88c566d118a190e310c
Lionel Henry <lionel.hry@gmail.com>
2017-05-18 12:07:29 +0200

    Move capture functions out of compat file

--- R/compat-oldrel.R
+++ R/compat-oldrel.R
@@ -1,38 +1,8 @@
-# nocov start - compat-oldrel (last updated: rlang 0.1.9000)
+# nocov start - compat-oldrel (last updated: rlang 0.1.2)
 
 # This file serves as a reference for compatibility functions for old
-# versions of R.
-
-
-# Compat function for capture functions (that will hopefully make
-# their way to the next R version) -----------------------------------
-
-if (TRUE || utils::packageVersion("base") < "3.4.0") {
-
-  captureArg <- function(x, strict = TRUE) {
-    caller_env <- parent.frame()
-
-    if (identical(caller_env, globalenv())) {
-      stop("must be called in a function")
-    }
-    if (missing(x)) {
-      stop("argument \"x\" is missing")
-    }
-
-    .Call(rlang_capturearg, NULL, NULL, pairlist(caller_env, strict), get_env())
-  }
-
-  captureDots <- function(strict = TRUE) {
-    caller_env <- parent.frame()
-
-    if (!exists("...", caller_env)) {
-      stop("must be called in a function where dots exist")
-    }
-
-    .Call(rlang_capturedots, NULL, NULL, pairlist(caller_env, strict), get_env())
-  }
-
-}
+# versions of R. Please find the most recent version in rlang's
+# repository.
 
 
 # R 3.2.0 ------------------------------------------------------------
