b543e8c2d2edbd60f170810df99849d439aea564
Lionel Henry <lionel.hry@gmail.com>
2017-06-15 13:34:58 +0200

    Add r_env_get() and r_base_env()

--- src/env.c
+++ src/env.c
@@ -1,5 +1,6 @@
 #include "rlang.h"
 
+
 SEXP r_mut_env_parent(SEXP env, SEXP new_parent) {
   SET_ENCLOS(env, new_parent);
   return env;
@@ -7,4 +8,12 @@ SEXP r_mut_env_parent(SEXP env, SEXP new_parent) {
 
 bool r_is_env(SEXP x) {
   return r_typeof(x) == ENVSXP;
+}
+
+SEXP r_env_get(SEXP env, SEXP sym) {
+  return Rf_findVarInFrame3(env, sym, TRUE);
+}
+
+SEXP r_base_env() {
+  return R_BaseEnv;
 }
--- src/env.h
+++ src/env.h
@@ -5,6 +5,8 @@
 
 
 bool r_is_env(SEXP x);
+SEXP r_env_get(SEXP env, SEXP sym);
+SEXP r_base_env();
 
 
 #endif
