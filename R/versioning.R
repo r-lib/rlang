c7ee78cad891b19d2d4b546d5cfe4cfc6a4297bf
Lionel Henry <lionel.hry@gmail.com>
2017-08-18 19:43:23 +0200

    Remove old deprecation signallers and export signal_deprecation()

--- R/versioning.R
+++ R/versioning.R
@@ -1,61 +1,12 @@
-#' Signal a deprecated or obsolete function
+#' Signal deprecation of a function or arguments
 #'
-#' @description
+#' @param .name The name of the function that is deprecated or whose
+#'   arguments are deprecated.
+#' @param .cycle A character vector defining the deprecation cycle.
+#' @param ... Replacements.
+#' @param .msg A custom error message.
 #'
-#' These functions signal a function as deprecated or obsolete.
-#'
-#' * A deprecated function does not issue a warning unless the
-#'   `rlang_verbose_deprecation` option is set to `TRUE`. This is
-#'   often the first stage towards obsolescence. It is recommended to
-#'   stop using deprecated functions and switch to suggested
-#'   replacements.
-#'
-#' * An obsolete function always issues a warning when called. Users
-#'   should stop using that function as it might get removed in the
-#'   future.
-#'
-#' @param name The name of the depreciated function as a string.
-#' @param replacement The name of the suggested replacement as a
-#'   string.
-#' @param start The package version where the function was
-#'   depreciated.
 #' @export
-#' @examples
-#' # A deprecated function is signalled silently:
-#' foo <- function() signal_deprecated("foo", "bar", "0.2.0")
-#' foo()
-#'
-#' # If the `rlang_verbose_deprecation` option is TRUE, a warning is
-#' # issued. If run, the following would throw a warning:
-#' #> options(rlang_verbose_deprecation = TRUE)
-#' #> foo()
-signal_deprecated <- function(name, replacement, start = NULL) {
-  if (is_true(peek_option("rlang_verbose_deprecation"))) {
-    signal <- cnd_warn
-  } else {
-    signal <- cnd_signal
-  }
-
-  msg <- deprecation_msg("deprecated", name, replacement, start)
-
-  signal("deprecated",
-    replacement = replacement,
-    start = start,
-    .msg = msg
-  )
-}
-#' @rdname signal_deprecated
-#' @export
-warn_obsolete <- function(name, replacement, start = NULL) {
-  msg <- deprecation_msg("obsolete", name, replacement, start)
-
-  cnd_warn("obsolete",
-    replacement = replacement,
-    start = start,
-    .msg = msg
-  )
-}
-
 signal_deprecation <- function(.name, .cycle, ..., .msg = NULL) {
   fn <- caller_fn()
   stopifnot(is_namespace(get_env(fn)))
