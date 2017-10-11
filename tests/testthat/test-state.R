9747b4f9b0bda9f06ec8b8131760cc93e6a83ad7
Lionel Henry <lionel.hry@gmail.com>
2017-10-11 23:23:38 +0200

    Add test for peek_option()

--- tests/testthat/test-state.R
+++ tests/testthat/test-state.R
@@ -7,6 +7,7 @@ test_that("can't add an exit event at top-level", {
 test_that("options are set temporarily", {
   scoped_options(foo = "foo")
   expect_identical(with_options(foo = "bar", peek_option("foo")), "bar")
+  expect_identical(peek_option("foo"), "foo")
 })
 
 test_that("peek_options() returns a named list", {
