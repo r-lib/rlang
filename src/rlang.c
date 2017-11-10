7df7370be1e3822eaa03695e9964a8bf19424073
Lionel Henry <lionel.hry@gmail.com>
2017-11-10 17:28:23 +0100

    Add note in implementation include point

--- src/rlang.c
+++ src/rlang.c
@@ -1,3 +1,7 @@
+// This is an include point for the implementations of the rlang
+// library. It should be included in a single and separate compilation
+// unit.
+
 #include "rlang/rlang.h"
 
 #include "rlang/attrs.c"
