97a8137c651b7599624f5ee88fc10a45cc92374a
Lionel Henry <lionel.hry@gmail.com>
2017-11-11 13:26:26 +0100

    Make node constructors static inline

--- src/rlang/node.c
+++ src/rlang/node.c
@@ -1,19 +1,1 @@
 #include "rlang.h"
-
-SEXP r_build_node(SEXP car, SEXP cdr) {
-  PROTECT(car);
-  PROTECT(cdr);
-  SEXP out = Rf_cons(car, cdr);
-  UNPROTECT(2);
-  return out;
-}
-
-SEXP r_build_pairlist(SEXP car) {
-  return r_build_node(car, r_null);
-}
-SEXP r_build_pairlist2(SEXP car1, SEXP car2) {
-  return r_build_node(car1, r_build_pairlist(car2));
-}
-SEXP r_build_pairlist3(SEXP car1, SEXP car2, SEXP car3) {
-  return r_build_node(car1, r_build_pairlist2(car2, car3));
-}
--- src/rlang/node.h
+++ src/rlang/node.h
@@ -85,10 +85,23 @@ SEXP r_new_node(SEXP car, SEXP cdr) {
   return Rf_cons(car, cdr);
 }
 
-SEXP r_build_node(SEXP car, SEXP cdr);
-SEXP r_build_pairlist(SEXP car);
-SEXP r_build_pairlist2(SEXP car1, SEXP car2);
-SEXP r_build_pairlist3(SEXP car1, SEXP car2, SEXP car3);
+static inline SEXP r_build_node(SEXP car, SEXP cdr) {
+  PROTECT(car);
+  PROTECT(cdr);
+  SEXP out = Rf_cons(car, cdr);
+  UNPROTECT(2);
+  return out;
+}
+
+static inline SEXP r_build_pairlist(SEXP car) {
+  return r_build_node(car, r_null);
+}
+static inline SEXP r_build_pairlist2(SEXP car1, SEXP car2) {
+  return r_build_node(car1, r_build_pairlist(car2));
+}
+static inline SEXP r_build_pairlist3(SEXP car1, SEXP car2, SEXP car3) {
+  return r_build_node(car1, r_build_pairlist2(car2, car3));
+}
 
 
 #endif
