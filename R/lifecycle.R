016746beb93b0677ac1ba1562b10c07fb1ac09fd
Lionel Henry <lionel.hry@gmail.com>
2017-12-05 16:31:31 +0100

    Add lifecycle documentation topics

---
+++ R/lifecycle.R
@@ -0,0 +1,43 @@
+#' Life cycle of the rlang package
+#'
+#' The rlang package is currently maturing. Unless otherwise stated,
+#' this applies to all its exported functions. See [lifecycle-stable]
+#' for a list of functions marked as stable.
+#'
+#' @name lifecycle
+NULL
+
+#' Stable functions
+#'
+#' TODO: Review all functions in rlang and mark stable ones as such.
+#'
+#' * [eval_tidy()]
+#' * [!!], [!!!]
+#' * [enquo()], [quo()], [quos()]
+#' * [enexpr()], [expr()], [exprs()]
+#' * [sym()], [syms()]
+#'
+#' * [set_names()]
+#' * [as_function()]
+#'
+#' @name lifecycle-stable
+NULL
+
+#' Soft-deprecated functions and arguments
+#'
+#' @section rlang 0.2.0:
+#'
+#' * [UQ()]
+#' * [UQS()]
+#'
+#' @name lifecycle-soft-deprecated
+NULL
+
+#' Deprecated functions and arguments
+#'
+#' @section rlang 0.2.0:
+#'
+#' * [UQE()]
+#'
+#' @name lifecycle-deprecated
+NULL
