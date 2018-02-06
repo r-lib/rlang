a11ca4c21d491e3093cac37b91d386a05b91473e
Lionel Henry <lionel.hry@gmail.com>
2018-02-06 06:35:39 -0800

    Don't use rlang functions during initialisation

--- R/c-api.R
+++ R/c-api.R
@@ -2,18 +2,21 @@
 c_constants_env <- new.env(parent = emptyenv())
 
 init_c_constants <- function() {
-  env_bind(c_constants_env,
-
+  constants <- list(
     tilde_thunk_fmls =
       formals(function(...) NULL),
 
     tilde_thunk_body =
-      expr((!!.Call)(!!(rlang_tilde_eval),
+      bquote(.(.Call)(.(rlang_tilde_eval),
         sys.call(),
         "data_mask_arg",
         "data_mask_top_arg",
         environment()
       ))
-
   )
+
+  nms <- names(constants)
+  for (i in seq_along(constants)) {
+    assign(nms[[i]], constants[[i]], envir = c_constants_env)
+  }
 }
