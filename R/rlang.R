6d01514276e48a0adffeeccd6115d26110580d07
Lionel Henry <lionel.hry@gmail.com>
2018-05-23 10:51:05 +0200

    Add experimental support for `.Last.error`

--- R/rlang.R
+++ R/rlang.R
@@ -4,7 +4,17 @@ NULL
 .onLoad <- function(lib, pkg) {
   init_c_constants()
   .Call(rlang_library_load)
+
+  while ("rlang" %in% search()) {
+    detach("rlang")
+  }
+  get("attach")(list(), name = "rlang")
+
+  set_last_error_binding()
 }
 .onUnload <- function(lib) {
   .Call(rlang_library_unload)
+  get("detach")("rlang")
 }
+
+rlang_attached_env <- function() scoped_env("rlang")
