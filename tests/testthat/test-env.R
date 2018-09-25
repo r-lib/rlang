6282a6920f2ff8b86de25049cd2ea58701a365e8
Lionel Henry <lionel.hry@gmail.com>
2018-09-25 13:53:31 +0200

    Split tests/testthat/test-env-special.R from tests/testthat/test-env.R 3/3

--- tests/testthat/test-env.R
+++ tests/testthat/test-env.R
@@ -53,19 +53,6 @@ test_that("locally() evaluates within correct environment", {
   expect_identical(env_parent(local_env), env)
 })
 
-test_that("ns_env() returns current namespace", {
-  expect_identical(with_env(ns_env("rlang"), ns_env()), get_env(rlang::get_env))
-})
-
-test_that("ns_imports_env() returns imports env", {
-  expect_identical(with_env(ns_env("rlang"), ns_imports_env()), env_parent(get_env(rlang::get_env)))
-})
-
-test_that("ns_env_name() returns namespace name", {
-  expect_identical(with_env(ns_env("base"), ns_env_name()), "base")
-  expect_identical(ns_env_name(rlang::get_env), "rlang")
-})
-
 test_that("as_environment() dispatches correctly", {
   expect_identical(as_environment("base"), base_env())
   expect_false(env_has(as_environment(set_names(letters)), "map"))
@@ -137,20 +124,6 @@ test_that("env_parents() returns all parents", {
   expect_identical(env_parents(env2), new_environments(list(env1, empty_env())))
 })
 
-test_that("scoped_envs() includes global and empty envs", {
-  envs <- scoped_envs()
-  expect_identical(envs[[1]], global_env())
-  expect_identical(envs[[length(envs)]], empty_env())
-})
-
-test_that("scoped_envs() returns named environments", {
-  expect_identical(names(scoped_envs()), scoped_names())
-})
-
-test_that("scoped_env() deals with empty environment", {
-  expect_identical(scoped_env("NULL"), empty_env())
-})
-
 test_that("env() doesn't partial match on env_bind()'s .env", {
   expect_true(all(env_has(env(.data = 1, . = 2), c(".data", "."))))
 })
@@ -215,11 +188,6 @@ test_that("friendly_env_type() returns a friendly env name", {
   expect_identical(friendly_env_type("local"), "a local environment")
 })
 
-test_that("is_namespace() recognises namespaces", {
-  expect_false(is_namespace(env()))
-  expect_true(is_namespace(get_env(is_namespace)))
-})
-
 test_that("new_environment() accepts optional parent", {
   env <- new_environment(parent = base_env())
   expect_reference(env_parent(env), base_env())
@@ -246,24 +214,6 @@ test_that("env_parents() stops at the global env by default", {
 test_that("env_parents() always stops at the empty env", {
   expect_identical(env_parents(empty_env()), new_environments(list()))
   expect_identical(env_parents(pkg_env("base")), new_environments(list(empty_env())))
-})
-
-test_that("env_name() returns proper environment name", {
-  expect_identical(env_name(global_env()), "global")
-  expect_identical(env_name(empty_env()), "empty")
-  expect_identical(env_name(base_env()), "base")
-
-  expect_identical(env_name(pkg_env("rlang")), "package:rlang")
-  expect_identical(env_name(ns_imports_env("rlang")), "imports:rlang")
-  expect_identical(env_name(ns_env("rlang")), "namespace:rlang")
-
-  env <- structure(env(), name = "foobar")
-  expect_identical(env_label(env), "foobar")
-})
-
-test_that("env_label() returns memory address for anonymous envs", {
-  env <- env()
-  expect_identical(env_label(env), sexp_address(env))
 })
 
 test_that("env_parents() returns a named list", {
