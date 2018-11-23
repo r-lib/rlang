7f5e32616d4a430a5b4551793696e5acf7f60889
Lionel Henry <lionel.hry@gmail.com>
2018-11-23 10:45:22 +0100

    Document lifecycle functions

--- R/compat-lifecycle.R
+++ R/compat-lifecycle.R
@@ -2,15 +2,27 @@
 
 # This file serves as a reference for currently unexported rlang
 # lifecycle functions. Please find the most recent version in rlang's
-# repository.
+# repository. These functions expect rlang to be imported in your
+# namespace.
 
 
 #' Signal deprecation
+#'
+#' @description
 #'
 #' `signal_soft_deprecated()` warns only if option is set, the package
 #'  is attached, or if called from the global environment.
 #'  `warn_deprecated()` warns unconditionally. Both functions warn
 #'  only once.
+#'
+#' Control the verbosity of retirement with scoped global options:
+#'
+#' * When `lifecycle_disable_verbose_retirement` is `TRUE`, no
+#'   warnings are issued in any circumstances.
+#'
+#' * When `lifecycle_force_verbose_retirement` is `TRUE`,
+#'   soft-deprecated functions always warn, unless
+#'   `lifecycle_disable_verbose_retirement` is `TRUE`.
 #'
 #' @param msg The deprecation message.
 #' @param id The id of the deprecation. A warning is issued only once
@@ -19,6 +31,8 @@
 #' @param package The soft-deprecation warning is forced when this
 #'   package is attached. Automatically detected from the caller
 #'   environment.
+#' @param env The environment in which the deprecated function was
+#'   called.
 #'
 #' @noRd
 NULL
@@ -78,6 +92,80 @@ deprecation_env <- new.env(parent = emptyenv())
 
 abort_defunct <- function(msg) {
   .Defunct(msg = msg)
+}
+
+
+#' Embed a lifecycle badge in documentation
+#'
+#' @description
+#'
+#' Use `lifecycle()` within a `Sexpr` macro to embed a
+#' [lifecycle](https://www.tidyverse.org/lifecycle/) badge in your
+#' documentation. The badge should appear first in the description:
+#'
+#' ```
+#' \Sexpr[results=rd, stage=render]{mypkg:::lifecycle("questioning")}
+#' ```
+#'
+#' The badge appears as an image in the HTML version of the
+#' documentation. To make them available in your package, visit
+#' <https://github.com/r-lib/rlang/tree/master/man/figures> and copy
+#' all the files starting with `lifecycle-` in your `man/figures/`
+#' folder.
+#'
+#' @param stage A lifecycle stage as a string, one of:
+#'   `"experimental"`, `"maturing"`, `"stable"`, `"questioning"`,
+#'   `"archived"`, `"soft-deprecated"`, `"deprecated"`, `"defunct"`.
+#'
+#' @noRd
+NULL
+
+lifecycle <- function(stage) {
+  url <- paste0("https://www.tidyverse.org/lifecycle/#", stage)
+  img <- lifecycle_img(stage, url)
+
+  sprintf(
+    "\\ifelse{html}{%s}{\\strong{%s}}",
+    img,
+    upcase1(stage)
+  )
+}
+
+lifecycle_img <- function(stage, url) {
+  file <- sprintf("lifecycle-%s.svg", stage)
+  stage_alt <- upcase1(stage)
+
+  switch(stage,
+
+    experimental = ,
+    maturing = ,
+    stable = ,
+    questioning = ,
+    archived =
+      sprintf(
+        "\\out{<a href='%s'><img src='%s' alt='%s lifecycle'></a>}",
+        url,
+        file.path("figures", file),
+        stage_alt
+      )
+   ,
+
+    `soft-deprecated` = ,
+    deprecated = ,
+    defunct =
+      sprintf(
+        "\\figure{%s}{options: alt='%s lifecycle'}",
+        file,
+        stage_alt
+      ),
+
+    abort(sprintf("Unknown lifecycle stage `%s`", stage))
+
+  )
+}
+upcase1 <- function(x) {
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  x
 }
 
 
