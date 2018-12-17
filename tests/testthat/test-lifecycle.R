c029912ed27913cb2ebc5f72c25cad0611af6819
Lionel Henry <lionel.hry@gmail.com>
2018-12-17 10:52:21 +0100

    Fix accidental redefinition of `with_lifecycle_warnings()`

--- tests/testthat/test-lifecycle.R
+++ tests/testthat/test-lifecycle.R
@@ -16,3 +16,9 @@ test_that("can promote lifecycle warnings to errors", {
   expect_error(signal_soft_deprecated("foo"), "foo")
   expect_error(warn_deprecated("foo"), "foo")
 })
+
+test_that("can enable warnings and errors with `with_` helpers", {
+  expect_warning(with_lifecycle_warnings(signal_soft_deprecated("foo")), "foo")
+  expect_error(with_lifecycle_errors(signal_soft_deprecated("foo")), "foo")
+  expect_no_warning(with_lifecycle_warnings(with_lifecycle_silence(warn_deprecated("foo"))))
+})
