fdadabafc156db7c929aebe82ba9301c5ba6e10e
Lionel Henry <lionel.hry@gmail.com>
2019-01-03 17:33:21 +0100

    Revert defunction of `as_dictionary()`
    To help with #674

--- R/lifecycle-retired.R
+++ R/lifecycle-retired.R
@@ -815,15 +815,17 @@ overscope_eval_next <- function(overscope, quo, env = base_env()) {
 #' @keywords internal
 #' @export
 as_dictionary <- function(x, lookup_msg = NULL, read_only = FALSE) {
-  stop_defunct(paste_line(
-    "`as_dictionary()` is defunct as of rlang 0.3.0.",
+  signal_soft_deprecated(paste_line(
+    "`as_dictionary()` is soft-deprecated as of rlang 0.3.0.",
     "Please use `as_data_pronoun()` instead"
   ))
+  as_data_pronoun(x)
 }
 #' @rdname dictionary
 #' @export
 is_dictionary <- function(x) {
-  stop_defunct("`is_dictionary()` is defunct as of rlang 0.3.0.")
+  stop_defunct("`is_dictionary()` is soft-deprecated as of rlang 0.3.0.")
+  inherits(x, "rlang_data_pronoun")
 }
 
 #' Test for or coerce to quosure-like objects
