a31e841f1625dbe6d4442e1d1aba1ff0f889376a
Lionel Henry <lionel.hry@gmail.com>
2019-04-09 14:35:31 +0200

    Rename `r_build_call()` to `r_call()`

--- src/lib/lang.h
+++ src/lib/lang.h
@@ -4,19 +4,10 @@
 #include "node.h"
 
 
-static inline sexp* r_new_call(sexp* car, sexp* cdr) {
-  return Rf_lcons(car, cdr);
-}
-
-static inline sexp* r_build_call(sexp* head) {
-  return r_new_call(head, r_null);
-}
-static inline sexp* r_build_call1(sexp* head, sexp* arg1) {
-  return r_new_call(head, r_pairlist(arg1));
-}
-static inline sexp* r_build_call2(sexp* head, sexp* arg1, sexp* arg2) {
-  return r_new_call(head, r_pairlist2(arg1, arg2));
-}
+#define r_new_call Rf_lcons
+#define r_call Rf_lang1
+#define r_call2 Rf_lang2
+#define r_call3 Rf_lang3
 
 bool r_is_call(sexp* x, const char* name);
 bool r_is_call_any(sexp* x, const char** names, int n);
