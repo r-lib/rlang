d2d01f171ac6d0878a85bb05fba1ae4163449962
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-04-19 08:53:22 -0700

    Rewrap

--- R/fn.R
+++ R/fn.R
@@ -394,10 +394,9 @@ fn_env <- function(fn) {
 #'   If a **formula**, e.g. `~ .x + 2`, it is converted to a function
 #'   with up to two arguments: `.x` (single argument) or `.x` and `.y`
 #'   (two arguments). The `.` placeholder can be used instead of `.x`.
-#'   This allows you to
-#'   create very compact anonymous functions with up to two inputs.
-#'   Functions created from formulas have a special class. Use
-#'   `is_lambda()` to test for it.
+#'   This allows you to create very compact anonymous functions with up
+#'   to two inputs. Functions created from formulas have a special
+#'   class. Use `is_lambda()` to test for it.
 #' @param env Environment in which to fetch the function in case `x`
 #'   is a string.
 #' @export
