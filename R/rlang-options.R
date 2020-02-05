41c3446382012a55312550d8c7b6523d7bedcfaf
Sean Ingerson <sean.ingerson@kiewit.com>
2020-02-05 16:46:30 -0600

    small tweaks to centralized option documentation.

--- R/rlang-options.R
+++ R/rlang-options.R
@@ -2,19 +2,19 @@
 #'
 #' @description
 #' rlang has several options which may be set globally to control
-#'  behavior. A brief description of each is given here. If any functions
-#'  are referenced, refer to their documentation for additional details.
+#' behavior. A brief description of each is given here. If any functions
+#' are referenced, refer to their documentation for additional details.
 #'
-#' * rlang_interactive: A logical value used by [is_interactive()]. This
+#' * `rlang_interactive`: A logical value used by [is_interactive()]. This
 #'    can be set to `TRUE` to test interactive behavior in unit tests,
 #'    for example.
-#' * rlang_backtrace_on_error: A character string which controls whether
+#' * `rlang_backtrace_on_error`: A character string which controls whether
 #'    backtraces are displayed with error messages, and the level of
 #'    detail they print. See [rlang_backtrace_on_error] for the possible option values.
-#' * rlang_trace_format_srcrefs: A logical value used to control whether
+#' * `rlang_trace_format_srcrefs`: A logical value used to control whether
 #'    srcrefs are printed as part of the backtrace.
-#' * rlang_trace_top_env: An environment which will be treated as the
+#' * `rlang_trace_top_env`: An environment which will be treated as the
 #'    top-level environment when printing traces. See [trace_back()]
 #'    for examples.
-#' @name rlang_options
+#' @name faq-options
 NULL
