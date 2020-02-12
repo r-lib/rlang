dcd41511436cc82bb249b7165e3d219da6b2d725
Lionel Henry <lionel.hry@gmail.com>
2020-02-12 14:19:53 +0100

    Fix windows-latest CI build

--- tests/testthat/test-trace.R
+++ tests/testthat/test-trace.R
@@ -613,8 +613,8 @@ test_that("fails when `bottom` is not on the stack", {
 })
 
 test_that("caught error does not display backtrace in knitted files", {
-  skip_if(!rmarkdown::pandoc_available())
   skip_if_not_installed("knitr")
+  skip_if(!rmarkdown::pandoc_available())
 
   local_options(
     rlang_backtrace_on_error = NULL,
