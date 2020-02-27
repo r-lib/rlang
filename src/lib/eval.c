ef943ff488db47c67ba2c8dc46e62cee6b298cba
DavisVaughan <davis@rstudio.com>
2020-02-27 10:22:14 -0500

    Extract out `r_eval_in_with_*()` helpers

--- src/lib/eval.c
+++ src/lib/eval.c
@@ -3,47 +3,75 @@
 
 sexp* r_eval_with_x(sexp* call, sexp* parent, sexp* x) {
   sexp* env = KEEP(r_new_environment(parent, 1));
-  r_env_poke(env, r_x_sym, x);
-
-  sexp* out = r_eval(call, env);
+  sexp* out = r_eval_in_with_x(call, env,
+                               x, r_x_sym);
 
   FREE(1);
   return out;
 }
 sexp* r_eval_with_xy(sexp* call, sexp* parent, sexp* x, sexp* y) {
   sexp* env = KEEP(r_new_environment(parent, 1));
-  r_env_poke(env, r_x_sym, x);
-  r_env_poke(env, r_y_sym, y);
-
-  sexp* out = r_eval(call, env);
+  sexp* out = r_eval_in_with_xy(call, env,
+                                x, r_x_sym,
+                                y, r_y_sym);
 
   FREE(1);
   return out;
 }
 sexp* r_eval_with_xyz(sexp* call, sexp* parent, sexp* x, sexp* y, sexp* z) {
   sexp* env = KEEP(r_new_environment(parent, 1));
-  r_env_poke(env, r_x_sym, x);
-  r_env_poke(env, r_y_sym, y);
-  r_env_poke(env, r_z_sym, z);
-
-  sexp* out = r_eval(call, env);
+  sexp* out = r_eval_in_with_xyz(call, env,
+                                 x, r_x_sym,
+                                 y, r_y_sym,
+                                 z, r_z_sym);
 
   FREE(1);
   return out;
 }
 sexp* r_eval_with_wxyz(sexp* call, sexp* parent, sexp* w, sexp* x, sexp* y, sexp* z) {
   sexp* env = KEEP(r_new_environment(parent, 1));
-  r_env_poke(env, r_w_sym, w);
-  r_env_poke(env, r_x_sym, x);
-  r_env_poke(env, r_y_sym, y);
-  r_env_poke(env, r_z_sym, z);
-
-  sexp* out = r_eval(call, env);
+  sexp* out = r_eval_in_with_wxyz(call, env,
+                                  w, r_w_sym,
+                                  x, r_x_sym,
+                                  y, r_y_sym,
+                                  z, r_z_sym);
 
   FREE(1);
   return out;
 }
 
+sexp* r_eval_in_with_x(sexp* call, sexp* env,
+                       sexp* x, sexp* x_sym) {
+  r_env_poke(env, x_sym, x);
+  return r_eval(call, env);
+}
+sexp* r_eval_in_with_xy(sexp* call, sexp* env,
+                        sexp* x, sexp* x_sym,
+                        sexp* y, sexp* y_sym) {
+  r_env_poke(env, x_sym, x);
+  r_env_poke(env, y_sym, y);
+  return r_eval(call, env);
+}
+sexp* r_eval_in_with_xyz(sexp* call, sexp* env,
+                         sexp* x, sexp* x_sym,
+                         sexp* y, sexp* y_sym,
+                         sexp* z, sexp* z_sym) {
+  r_env_poke(env, x_sym, x);
+  r_env_poke(env, y_sym, y);
+  r_env_poke(env, z_sym, z);
+  return r_eval(call, env);
+}
+sexp* r_eval_in_with_wxyz(sexp* call, sexp* env,
+                          sexp* w, sexp* w_sym,
+                          sexp* x, sexp* x_sym,
+                          sexp* y, sexp* y_sym,
+                          sexp* z, sexp* z_sym) {
+  r_env_poke(env, w_sym, w);
+  r_env_poke(env, x_sym, x);
+  r_env_poke(env, y_sym, y);
+  r_env_poke(env, z_sym, z);
+  return r_eval(call, env);
+}
 
 static sexp* shared_x_env;
 static sexp* shared_xy_env;
