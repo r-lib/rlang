7f17e0202cdfd11de247a9971296acec524f92ab
Kirill Müller <krlmlr+r@mailbox.org>
2020-06-16 05:54:40 +0200

    Deprecate values argument to arg_match()

--- tests/testthat/test-arg.R
+++ tests/testthat/test-arg.R
@@ -2,44 +2,24 @@ context("arg")
 
 test_that("matches arg", {
   myarg <- "foo"
-  expect_identical(arg_match(myarg, c("bar", "foo")), "foo")
+  expect_identical(arg_match0(myarg, c("bar", "foo")), "foo")
   expect_error(
-    arg_match(myarg, c("bar", "baz")),
+    arg_match0(myarg, c("bar", "baz")),
     "`myarg` must be one of \"bar\" or \"baz\""
   )
 })
 
 test_that("gives an error with more than one arg", {
-  myarg <- c("bar","fun")
+  myarg <- c("bar", "fun")
   expect_error(
-    regex = "`myarg` must be one of \"bar\" or \"baz\".",
-    arg_match(myarg, c("bar", "baz"))
+    regex = "`myarg` must be a string.",
+    arg_match0(myarg, c("bar", "baz"))
   )
 })
 
-test_that("gives error with different than rearranged arg vs value", {
-  f <- function(myarg = c("foo", "bar", "fun")) {
-    arg_match(myarg, c("fun", "bar"))
-  }
-  expect_error(f(), regex = "`myarg` must be one of \"fun\" or \"bar\"")
-})
-
-test_that("gives no error with rearranged arg vs value", {
-  f <- function(myarg = c("bar", "fun")) {
-    arg_match(myarg, c("fun", "bar"))
-  }
-  expect_identical(f(), "bar")
-})
-
-test_that("uses first value when called with all values", {
-  myarg <- c("bar","baz")
-  expect_identical(arg_match(myarg, c("bar", "baz")), "bar")
-})
-
 test_that("informative error message on partial match", {
-  myarg <- "f"
   expect_error(
-    arg_match(myarg, c("bar", "foo")),
+    arg_match0("f", c("bar", "foo")),
     "Did you mean \"foo\"?"
   )
 })
@@ -47,27 +27,30 @@ test_that("informative error message on partial match", {
 test_that("informative error message on a typo", {
   verify_output("test-typo-suggest.txt", {
     myarg <- "continuuos"
-    arg_match(myarg, c("discrete", "continuous"))
+    arg_match0(myarg, c("discrete", "continuous"))
     myarg <- "fou"
-    arg_match(myarg, c("bar", "foo"))
+    arg_match0(myarg, c("bar", "foo"))
     myarg <- "fu"
-    arg_match(myarg, c("ba", "fo"))
+    arg_match0(myarg, c("ba", "fo"))
 
     "# No suggestion when the edit distance is too large"
     myarg <- "foobaz"
-    arg_match(myarg, c("fooquxs", "discrete"))
+    arg_match0(myarg, c("fooquxs", "discrete"))
     myarg <- "a"
-    arg_match(myarg, c("b", "c"))
+    arg_match0(myarg, c("b", "c"))
 
     "# Even with small possible typos, if there's a match it returns the match"
     myarg <- "bas"
-    arg_match(myarg, c("foo", "baz", "bas"))
+    arg_match0(myarg, c("foo", "baz", "bas"))
   })
 })
 
 test_that("gets choices from function", {
-  fn <- function(myarg = c("bar", "foo")) arg_match(myarg)
+  fn <- function(myarg = c("bar", "foo")) {
+    arg_match(myarg)
+  }
   expect_error(fn("f"), "Did you mean \"foo\"?")
+  expect_identical(fn(), "bar")
   expect_identical(fn("foo"), "foo")
 })
 
