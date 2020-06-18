c02d5dca728b2b2262ec73f94ba19f5538575e90
Lionel Henry <lionel.hry@gmail.com>
2020-06-18 13:53:46 +0200

    Supersede `env_bury()` officially

--- R/env-binding.R
+++ R/env-binding.R
@@ -304,41 +304,6 @@ with_bindings <- function(.expr, ..., .env = caller_env()) {
   .expr
 }
 
-#' Mask bindings by defining symbols deeper in a scope
-#'
-#' `env_bury()` is like [env_bind()] but it creates the bindings in a
-#' new child environment. This makes sure the new bindings have
-#' precedence over old ones, without altering existing environments.
-#' Unlike `env_bind()`, this function does not have side effects and
-#' returns a new environment (or object wrapping that environment).
-#'
-#' @inheritParams env_bind
-#' @return A copy of `.env` enclosing the new environment containing
-#'   bindings to `...` arguments.
-#' @seealso [env_bind()], [env_unbind()]
-#'
-#' @keywords internal
-#' @export
-#' @examples
-#' orig_env <- env(a = 10)
-#' fn <- set_env(function() a, orig_env)
-#'
-#' # fn() currently sees `a` as the value `10`:
-#' fn()
-#'
-#' # env_bury() will bury the current scope of fn() behind a new
-#' # environment:
-#' fn <- env_bury(fn, a = 1000)
-#' fn()
-#'
-#' # Even though the symbol `a` is still defined deeper in the scope:
-#' orig_env$a
-env_bury <- function(.env, ...) {
-  env_ <- get_env(.env)
-  env_ <- child_env(env_, ...)
-  set_env(.env, env_)
-}
-
 #' Remove bindings from an environment
 #'
 #' `env_unbind()` is the complement of [env_bind()]. Like `env_has()`,
