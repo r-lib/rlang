f4a382fd49363ac014df6ba68ef9952eecae4fa7
Lionel Henry <lionel.hry@gmail.com>
2020-08-12 11:15:28 +0200

    Review style in attrs.h

--- src/lib/attrs.h
+++ src/lib/attrs.h
@@ -4,10 +4,12 @@
 #include "sym.h"
 
 
-static inline sexp* r_attrib(sexp* x) {
+static inline
+sexp* r_attrib(sexp* x) {
   return ATTRIB(x);
 }
-inline sexp* r_poke_attrib(sexp* x, sexp* attrs) {
+static inline
+sexp* r_poke_attrib(sexp* x, sexp* attrs) {
   SET_ATTRIB(x, attrs);
   return x;
 }
@@ -16,7 +18,8 @@ sexp* r_attrib_get(sexp* x, sexp* tag);
 sexp* r_attrib_push(sexp* x, sexp* tag, sexp* value);
 sexp* r_attrib_set(sexp* x, sexp* tag, sexp* value);
 
-static inline void r_push_names(sexp* x, sexp* value) {
+static inline
+void r_push_names(sexp* x, sexp* value) {
   r_attrib_push(x, R_NamesSymbol, value);
 }
 
@@ -26,14 +29,17 @@ sexp* r_node_push_class(sexp* x, const char* tag);
 void r_push_classes(sexp* x, const char** tags);
 void r_push_class(sexp* x, const char* tag);
 
-static inline void r_poke_attribute(sexp* x, sexp* sym, sexp* value) {
+static inline
+void r_poke_attribute(sexp* x, sexp* sym, sexp* value) {
   Rf_setAttrib(x, sym, value);
 }
-static inline void r_poke_class(sexp* x, sexp* classes) {
+static inline
+void r_poke_class(sexp* x, sexp* classes) {
   r_poke_attribute(x, r_class_sym, classes);
 }
 
-static inline sexp* r_set_class(sexp* x, sexp* classes) {
+static inline
+sexp* r_set_class(sexp* x, sexp* classes) {
   x = r_attrib_set(x, r_class_sym, classes);
 
   if (classes == r_null) {
@@ -44,14 +50,17 @@ static inline sexp* r_set_class(sexp* x, sexp* classes) {
 
   return x;
 }
-static inline sexp* r_class(sexp* x) {
+static inline
+sexp* r_class(sexp* x) {
   return r_attrib_get(x, r_class_sym);
 }
 
-static inline sexp* r_names(sexp* x) {
+static inline
+sexp* r_names(sexp* x) {
   return r_attrib_get(x, r_names_sym);
 }
-static inline void r_poke_names(sexp* x, sexp* nms) {
+static inline
+void r_poke_names(sexp* x, sexp* nms) {
   r_poke_attribute(x, r_names_sym, nms);
 }
 
