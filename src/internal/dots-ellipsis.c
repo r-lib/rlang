4351bdcec1fecb5008a4e4b0524f89f5147a1281
Lionel Henry <lionel.hry@gmail.com>
2020-11-05 16:23:38 +0100

    Use rlang internals in ellipsis implementations

--- src/internal/dots-ellipsis.c
+++ src/internal/dots-ellipsis.c
@@ -1,99 +1,92 @@
-#define R_NO_REMAP
-#include <R.h>
-#include <Rinternals.h>
-#include <stdio.h>
-#include <stdbool.h>
+#include "rlang.h"
 
 static
-SEXP find_dots(SEXP env) {
-  if (TYPEOF(env) != ENVSXP) {
-    Rf_errorcall(R_NilValue, "`env` is a not an environment");
+r_obj* ffi_ellipsis_find_dots(r_obj* env) {
+  if (r_typeof(env) != R_TYPE_environment) {
+    r_abort("`env` is a not an environment.");
   }
 
-  SEXP dots = PROTECT(Rf_findVarInFrame3(env, R_DotsSymbol, TRUE));
-  if (dots == R_UnboundValue) {
-    Rf_errorcall(R_NilValue, "No ... found");
+  r_obj* dots = KEEP(r_env_find(env, r_syms.dots));
+  if (dots == r_syms.unbound) {
+    r_abort("No `...` found.");
   }
 
-  UNPROTECT(1);
+  FREE(1);
   return dots;
 }
 
-SEXP ffi_ellipsis_dots(SEXP env, SEXP auto_name_) {
-  int auto_name = Rf_asLogical(auto_name_);
-
-  SEXP dots = PROTECT(find_dots(env));
+r_obj* ffi_ellipsis_dots(r_obj* env, r_obj* auto_name) {
+  r_obj* dots = ffi_ellipsis_find_dots(env);
 
   // Empty dots
-  if (dots == R_MissingArg) {
-    UNPROTECT(1);
-    return Rf_allocVector(VECSXP, 0);
+  if (dots == r_syms.missing) {
+    return r_globals.empty_list;
   }
 
-  R_len_t n = Rf_length(dots);
+  KEEP(dots);
 
-  SEXP out = PROTECT(Rf_allocVector(VECSXP, n));
-  SEXP names = PROTECT(Rf_allocVector(STRSXP, n));
-  Rf_setAttrib(out, R_NamesSymbol, names);
+  bool c_auto_name = r_as_bool(auto_name);
 
-  for (R_len_t i = 0; i < n; ++i) {
-    SET_VECTOR_ELT(out, i, CAR(dots));
+  int n = r_length(dots);
+  r_obj* out = KEEP(r_alloc_list(n));
 
-    SEXP name = TAG(dots);
-    if (TYPEOF(name) == SYMSXP) {
-      SET_STRING_ELT(names, i, PRINTNAME(name));
+  r_obj* names = r_alloc_character(n);
+  r_attrib_poke(out, r_syms.names, names);
+
+  for (int i = 0; i < n; ++i) {
+    r_list_poke(out, i, r_node_car(dots));
+
+    r_obj* name = r_node_tag(dots);
+    if (r_typeof(name) == R_TYPE_symbol) {
+      r_chr_poke(names, i, r_sym_string(name));
     } else {
-      if (auto_name) {
+      if (c_auto_name) {
         char buffer[20];
         snprintf(buffer, 20, "..%i", i + 1);
-        SET_STRING_ELT(names, i, Rf_mkChar(buffer));
+        r_chr_poke(names, i, r_str(buffer));
       } else {
-        SET_STRING_ELT(names, i, NA_STRING);
+        r_chr_poke(names, i, r_globals.na_str);
       }
     }
 
-    dots = CDR(dots);
+    dots = r_node_cdr(dots);
   }
 
-  UNPROTECT(3);
+  FREE(2);
   return out;
 }
 
 static
-bool promise_forced(SEXP x) {
-  if (TYPEOF(x) != PROMSXP) {
+bool ellipsis_promise_forced(r_obj* x) {
+  if (r_typeof(x) != R_TYPE_promise) {
     return true;
   } else {
-    return PRVALUE(x) != R_UnboundValue;
+    return PRVALUE(x) != r_syms.unbound;
   }
 }
-SEXP ffi_ellipsis_promise_forced(SEXP x) {
-  return Rf_ScalarLogical(promise_forced(x));
+r_obj* ffi_ellipsis_promise_forced(r_obj* x) {
+  return r_lgl(ellipsis_promise_forced(x));
 }
 
-SEXP ffi_ellipsis_dots_used(SEXP env) {
-  SEXP dots = PROTECT(find_dots(env));
+r_obj* ffi_ellipsis_dots_used(r_obj* env) {
+  r_obj* dots = KEEP(ffi_ellipsis_find_dots(env));
 
-  if (dots == R_MissingArg) {
-    UNPROTECT(1);
-    return Rf_ScalarLogical(true);
+  if (dots == r_syms.missing) {
+    FREE(1);
+    return r_true;
   }
 
-  while (dots != R_NilValue) {
-    SEXP elt = CAR(dots);
+  while (dots != r_null) {
+    r_obj* elt = r_node_car(dots);
 
-    if (!promise_forced(elt)) {
-      UNPROTECT(1);
-      return Rf_ScalarLogical(false);
+    if (!ellipsis_promise_forced(elt)) {
+      FREE(1);
+      return r_false;
     }
 
-    dots = CDR(dots);
+    dots = r_node_cdr(dots);
   }
 
-  UNPROTECT(1);
-  return Rf_ScalarLogical(true);
-}
-
-SEXP ellipsis_eval_bare(SEXP expr, SEXP env) {
-  return Rf_eval(expr, env);
+  FREE(1);
+  return r_true;
 }
