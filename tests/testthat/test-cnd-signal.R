a96f284da9a8bd316842f8f5458515105c4efc7e
Lionel Henry <lionel.hry@gmail.com>
2020-11-21 08:24:42 +0100

    Send messages to `stderr` when a sink is active (#1066)
    Closes #1037

--- tests/testthat/test-cnd-signal.R
+++ tests/testthat/test-cnd-signal.R
@@ -183,6 +183,20 @@ test_that("cnd_signal() is a no-op with `NULL`", {
   expect_null(catch_cnd(cnd_signal(NULL)))
 })
 
+test_that("`inform()` behaves consistently in interactive and non-interactive sessions (#1037)", {
+  # Default behaviour
+  out1 <- Rscript(shQuote(c("--vanilla", "-e", "rlang::inform('foo')")))
+  out2 <- Rscript(shQuote(c("--vanilla", "-e", "rlang::with_interactive(rlang::inform('foo'))")))
+  expect_equal(out1$out, "foo")
+  expect_equal(out1$out, out2$out)
+
+  # Sinked behaviour
+  out1 <- Rscript(shQuote(c("--vanilla", "-e", "capture.output(rlang::inform('foo'))")))
+  out2 <- Rscript(shQuote(c("--vanilla", "-e", "rlang::with_interactive(capture.output(rlang::inform('foo')))")))
+  expect_equal(out1$out, c("foo", "character(0)"))
+  expect_equal(out1$out, out2$out)
+})
+
 
 # Lifecycle ----------------------------------------------------------
 
