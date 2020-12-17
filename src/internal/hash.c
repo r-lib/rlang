7b260f756d9a5a4b52c089d7c5761a77535d57d1
DavisVaughan <davis@rstudio.com>
2020-12-17 15:25:03 -0500

    Remove the comment about `XXH_INLINE_ALL`
    We have now figured out how to avoid the issue over in https://github.com/Cyan4973/xxHash/issues/482, but it doesn't seem to be any faster here so we can stick with the standard defines

--- src/internal/hash.c
+++ src/internal/hash.c
@@ -3,12 +3,6 @@
 /*
  * Using the standard xxhash defines, as seen in:
  * https://github.com/Cyan4973/xxHash/blob/4c881f796d6af27ef7d9c48f87817da0d3d75dc1/xxhash.c#L40-L41
- *
- * `XXH_INLINE_ALL` is an alternative, but it does not seem to be any faster for
- * our use cases. Additionally, on 32-bit Windows with Rtools40
- * (i.e. gcc 8.3.0), a significant warning is thrown when
- * `XXH_INLINE_ALL` is used.
- * https://github.com/Cyan4973/xxHash/issues/482
  */
 #define XXH_STATIC_LINKING_ONLY
 #define XXH_IMPLEMENTATION
