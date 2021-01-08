786aa81d34ebec0e8dccfc16c72769632ea4379c
Lionel Henry <lionel.hry@gmail.com>
2021-01-08 15:49:46 +0100

    Move `r_is_named()` to attrib.c

--- src/rlang/attrib.c
+++ src/rlang/attrib.c
@@ -115,3 +115,17 @@ void r_push_class(sexp* x, const char* tag) {
   tags[0] = tag;
   r_push_classes(x, tags);
 }
+
+bool r_is_named(sexp* x) {
+  sexp* nms = r_names(x);
+
+  if (r_typeof(nms) != STRSXP) {
+    return false;
+  }
+
+  if (r_chr_has(nms, "")) {
+    return false;
+  }
+
+  return true;
+}
--- src/rlang/sexp.c
+++ src/rlang/sexp.c
@@ -1,15 +1,2 @@
 #include "rlang.h"
 
-bool r_is_named(sexp* x) {
-  sexp* nms = r_names(x);
-
-  if (r_typeof(nms) != STRSXP) {
-    return false;
-  }
-
-  if (r_chr_has(nms, "")) {
-    return false;
-  }
-
-  return true;
-}
