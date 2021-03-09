51ff1a19b32969af8e8c12b9ea23061cf67d9566
Lionel Henry <lionel.hry@gmail.com>
2021-03-09 15:03:36 +0100

    Use rlang C lib in eval-tidy.c

--- src/internal/eval-tidy.c
+++ src/internal/eval-tidy.c
@@ -215,7 +215,7 @@ static sexp* mask_find(sexp* env, sexp* sym) {
   sexp* cur = env;
   do {
     sexp* obj = r_env_find(cur, sym);
-    if (TYPEOF(obj) == PROMSXP) {
+    if (r_typeof(obj) == R_TYPE_promise) {
       KEEP(obj);
       obj = r_eval(obj, r_empty_env);
       FREE(1);
