2d277e3f61caa42a3400e020281aeb0f4cb202a1
Hadley Wickham <h.wickham@gmail.com>
2021-04-15 12:04:10 -0500

    Add changelog

--- R/compat-purrr.R
+++ R/compat-purrr.R
@@ -1,10 +1,16 @@
-# nocov start - compat-purrr (last updated: rlang 0.3.2.9000)
+# nocov start - compat-purrr.R
+# Latest version: https://github.com/r-lib/rlang/blob/master/R/compat-purrr.R
 
-# This file serves as a reference for compatibility functions for
-# purrr. They are not drop-in replacements but allow a similar style
-# of programming. This is useful in cases where purrr is too heavy a
-# package to depend on. Please find the most recent version in rlang's
-# repository.
+# This file provides a minimal shim to provide a purrr-like API on top of
+# base R functions. They are not drop-in replacements but allow a similar style
+# of programming.
+#
+# Changelog:
+# 2020-04-14:
+# * removed pluck*() functions
+# * removed *_cpl() functions
+# * used as_function() to allow use of ~
+# * used . prefix for helpers
 
 map <- function(.x, .f, ...) {
   .f <- as_function(.f)
