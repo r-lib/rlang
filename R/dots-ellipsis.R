1a53bd085c8e0ed41dcc56cce334dde926da7067
Lionel Henry <lionel.hry@gmail.com>
2021-04-30 11:39:34 +0200

    Let signal functions format bullets for ellipsis errors

--- R/dots-ellipsis.R
+++ R/dots-ellipsis.R
@@ -161,13 +161,13 @@ check_dots_empty0 <- function(...) {
 }
 
 action_dots <- function(action, message, dot_names, note = NULL, class = NULL, ...) {
-  message <- format_bullets(c(
+  message <- c(
     message,
     i = note,
     x = "We detected these problematic arguments:",
     set_names(chr_quoted(dot_names), "*"),
     i = "Did you misspecify an argument?"
-  ))
+  )
   action(message, class = c(class, "rlib_error_dots"), ...)
 }
 
