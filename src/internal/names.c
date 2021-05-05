3e7d0dade0173e91107bc191c5d5964dd8061716
Lionel Henry <lionel.hry@gmail.com>
2021-05-05 11:40:48 +0200

    Update `names.c` to rlang style

--- src/internal/names.c
+++ src/internal/names.c
@@ -2,122 +2,59 @@
 #include <ctype.h>
 #include "internal.h"
 
+#include "decl/names-decl.h"
+
 // 3 leading '.' + 1 trailing '\0' + 24 characters
 #define MAX_IOTA_SIZE 28
 
-static
-bool is_unique_names(SEXP names);
 
-SEXP as_unique_names_impl(SEXP names, bool quiet);
-
-static
-ptrdiff_t suffix_pos(const char* name);
-
-static
-bool needs_suffix(SEXP str);
-
-// TODO: Export as well?
-static
-void describe_repair(SEXP old_names, SEXP new_names);
-
-bool any_has_suffix(SEXP names) {
-  R_len_t n = Rf_length(names);
-  const SEXP* names_ptr = STRING_PTR_RO(names);
-
-  for (R_len_t i = 0; i < n; ++i) {
-    SEXP elt = names_ptr[i];
-
-    if (suffix_pos(CHAR(elt)) >= 0) {
-      return true;
-    }
-  }
-
-  return false;
+r_obj* ffi_chr_as_unique_names(r_obj* names, r_obj* quiet) {
+  return vec_as_unique_names(names, r_lgl_get(quiet, 0));
 }
 
-SEXP vec_as_unique_names(SEXP names, bool quiet) {
+r_obj* vec_as_unique_names(r_obj* names, bool quiet) {
   if (is_unique_names(names) && !any_has_suffix(names)) {
     return names;
-  } else {
-    return(as_unique_names_impl(names, quiet));
-  }
-}
-
-SEXP ffi_chr_as_unique_names(SEXP names, SEXP quiet) {
-  SEXP out = PROTECT(vec_as_unique_names(names, LOGICAL(quiet)[0]));
-  UNPROTECT(1);
-  return out;
-}
-
-static
-bool is_unique_names(SEXP names) {
-  if (TYPEOF(names) != STRSXP) {
-    Rf_errorcall(R_NilValue, "`names` must be a character vector");
   }
 
-  R_len_t n = Rf_length(names);
-  const SEXP* names_ptr = STRING_PTR_RO(names);
+  r_ssize n = r_length(names);
 
-  if (Rf_any_duplicated(names, FALSE)) {
-    return false;
-  }
+  r_obj* new_names = KEEP(r_clone(names));
+  r_obj* const * v_new_names = r_chr_cbegin(new_names);
 
-  for (R_len_t i = 0; i < n; ++i) {
-    SEXP elt = names_ptr[i];
-
-    if (needs_suffix(elt)) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-static
-void stop_large_name() {
-  Rf_errorcall(R_NilValue, "Can't tidy up name because it is too large");
-}
-
-SEXP as_unique_names_impl(SEXP names, bool quiet) {
-  R_len_t n = Rf_length(names);
-
-  SEXP new_names = PROTECT(Rf_shallow_duplicate(names));
-  const SEXP* new_names_ptr = STRING_PTR_RO(new_names);
-
-  for (R_len_t i = 0; i < n; ++i) {
-    SEXP elt = new_names_ptr[i];
+  for (r_ssize i = 0; i < n; ++i) {
+    r_obj* elt = v_new_names[i];
 
     // Set `NA` and dots values to "" so they get replaced by `...n`
     // later on
     if (needs_suffix(elt)) {
-      elt = r_strs.empty;
-      SET_STRING_ELT(new_names, i, elt);
+      r_chr_poke(new_names, i, r_strs.empty);
       continue;
     }
 
     // Strip `...n` suffixes
-    const char* nm = CHAR(elt);
+    const char* nm = r_str_c_string(elt);
     int pos = suffix_pos(nm);
     if (pos >= 0) {
       elt = Rf_mkCharLenCE(nm, pos, Rf_getCharCE(elt));
-      SET_STRING_ELT(new_names, i, elt);
+      r_chr_poke(new_names, i, elt);
       continue;
     }
   }
 
   // Append all duplicates with a suffix
 
-  SEXP dups = PROTECT(chr_detect_dups(new_names));
-  const int* dups_ptr = LOGICAL_RO(dups);
+  r_obj* dups = KEEP(chr_detect_dups(new_names));
+  const int* dups_ptr = r_lgl_cbegin(dups);
 
-  for (R_len_t i = 0; i < n; ++i) {
-    SEXP elt = new_names_ptr[i];
+  for (r_ssize i = 0; i < n; ++i) {
+    r_obj* elt = v_new_names[i];
 
     if (elt != r_strs.empty && !dups_ptr[i]) {
       continue;
     }
 
-    const char* name = CHAR(elt);
+    const char* name = r_str_c_string(elt);
 
     int size = strlen(name);
     int buf_size = size + MAX_IOTA_SIZE;
@@ -129,40 +66,60 @@ SEXP as_unique_names_impl(SEXP names, bool quiet) {
     memcpy(buf, name, size);
     int remaining = buf_size - size;
 
-    int needed = snprintf(buf + size, remaining, "...%d", i + 1);
+    int needed = snprintf(buf + size, remaining, "...%ld", i + 1);
     if (needed >= remaining) {
       stop_large_name();
     }
 
-    SET_STRING_ELT(new_names, i, Rf_mkCharLenCE(buf, size + needed, Rf_getCharCE(elt)));
+    r_chr_poke(new_names, i, Rf_mkCharLenCE(buf, size + needed, Rf_getCharCE(elt)));
   }
 
   if (!quiet) {
     describe_repair(names, new_names);
   }
 
-  UNPROTECT(2);
+  FREE(2);
   return new_names;
 }
 
 static
-bool is_dotdotint(const char* name) {
-  int n = strlen(name);
+bool is_unique_names(r_obj* names) {
+  if (r_typeof(names) != R_TYPE_character) {
+    r_abort("`names` must be a character vector.");
+  }
 
-  if (n < 3) {
+  r_ssize n = r_length(names);
+  r_obj* const * v_names = r_chr_cbegin(names);
+
+  if (Rf_any_duplicated(names, FALSE)) {
     return false;
   }
-  if (name[0] != '.' || name[1] != '.') {
-    return false;
+
+  for (r_ssize i = 0; i < n; ++i) {
+    r_obj* elt = v_names[i];
+
+    if (needs_suffix(elt)) {
+      return false;
+    }
   }
 
-  if (name[2] == '.') {
-    name += 3;
-  } else {
-    name += 2;
+  return true;
+}
+
+static
+bool any_has_suffix(r_obj* names) {
+  r_ssize n = r_length(names);
+  r_obj* const * v_names = r_chr_cbegin(names);
+
+  for (r_ssize i = 0; i < n; ++i) {
+    const char* elt = r_str_c_string(v_names[i]);
+
+    if (suffix_pos(elt) >= 0) {
+      return true;
+    }
   }
 
-  return (bool) strtol(name, NULL, 10);
+  return false;
 }
 
 static
@@ -226,22 +183,44 @@ ptrdiff_t suffix_pos(const char* name) {
 }
 
 static
-bool needs_suffix(SEXP str) {
+bool needs_suffix(r_obj* str) {
   return
-    str == NA_STRING ||
+    str == r_strs.na ||
     str == r_strs.dots ||
     str == r_strs.empty ||
-    is_dotdotint(CHAR(str));
+    is_dotdotint(r_str_c_string(str));
 }
 
 static
-void describe_repair(SEXP old_names, SEXP new_names) {
-  SEXP call = PROTECT(Rf_lang3(Rf_install("describe_repair"),
-    old_names, new_names));
-  Rf_eval(call, rlang_ns_env);
+bool is_dotdotint(const char* name) {
+  int n = strlen(name);
 
-  // To reset visibility when called from a `.External2()`
-  Rf_eval(R_NilValue, R_EmptyEnv);
+  if (n < 3) {
+    return false;
+  }
+  if (name[0] != '.' || name[1] != '.') {
+    return false;
+  }
 
-  UNPROTECT(1);
+  if (name[2] == '.') {
+    name += 3;
+  } else {
+    name += 2;
+  }
+
+  return (bool) strtol(name, NULL, 10);
+}
+
+
+// TODO: Export as well?
+static
+void describe_repair(r_obj* old_names, r_obj* new_names) {
+  r_obj* call = KEEP(r_call3(r_sym("describe_repair"), old_names, new_names));
+  r_eval(call, rlang_ns_env);
+  FREE(1);
+}
+
+static
+void stop_large_name() {
+  r_abort("Can't tidy up name because it is too large.");
 }
