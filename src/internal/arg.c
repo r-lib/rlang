4181a6228f1df72ff7c4d85669048858ccec7f19
Lionel Henry <lionel.hry@gmail.com>
2021-05-05 09:25:01 +0200

    Use `v_` prefix for arrays

--- src/internal/arg.c
+++ src/internal/arg.c
@@ -98,13 +98,13 @@ int arg_match(r_obj* arg, r_obj* values, r_obj* arg_nm) {
     r_abort("`%s` must be a string or have the same length as `values`.", unwrap_c_str(arg_nm));
   }
 
-  r_obj* const* p_values = r_chr_cbegin(values);
+  r_obj* const* v_values = r_chr_cbegin(values);
 
   // Simple case: one argument, we check if it's one of the values.
   if (arg_len == 1) {
     r_obj* arg_char = r_chr_get(arg, 0);
     for (int i = 0; i < values_len; ++i) {
-      if (arg_char == p_values[i]) {
+      if (arg_char == v_values[i]) {
         return i;
       }
     }
@@ -113,12 +113,12 @@ int arg_match(r_obj* arg, r_obj* values, r_obj* arg_nm) {
     r_stop_unreached("arg_match");
   }
 
-  r_obj* const* p_arg = r_chr_cbegin(arg);
+  r_obj* const* v_arg = r_chr_cbegin(arg);
 
   // Same-length vector: must be identical, we allow changed order.
   int i = 0;
   for (; i < arg_len; ++i) {
-    if (p_arg[i] != p_values[i]) {
+    if (v_arg[i] != v_values[i]) {
       break;
     }
   }
@@ -129,22 +129,22 @@ int arg_match(r_obj* arg, r_obj* values, r_obj* arg_nm) {
   }
 
   r_obj* my_values = KEEP(r_clone(values));
-  r_obj* const * p_my_values = r_chr_cbegin(my_values);
+  r_obj* const * v_my_values = r_chr_cbegin(my_values);
 
   // Invariant: my_values[i:(len-1)] contains the values we haven't matched yet
   for (; i < arg_len; ++i) {
-    r_obj* current_arg = p_arg[i];
-    if (current_arg == p_my_values[i]) {
+    r_obj* current_arg = v_arg[i];
+    if (current_arg == v_my_values[i]) {
       continue;
     }
 
     bool matched = false;
     for (int j = i + 1; j < arg_len; ++j) {
-      if (current_arg == p_my_values[j]) {
+      if (current_arg == v_my_values[j]) {
         matched = true;
 
         // Replace matched value by the element that failed to match at this iteration
-        r_chr_poke(my_values, j, p_my_values[i]);
+        r_chr_poke(my_values, j, v_my_values[i]);
         break;
       }
     }
@@ -159,7 +159,7 @@ int arg_match(r_obj* arg, r_obj* values, r_obj* arg_nm) {
 
   r_obj* first_elt = r_chr_get(arg, 0);
   for (i = 0; i < values_len; ++i) {
-    if (first_elt == p_values[i]) {
+    if (first_elt == v_values[i]) {
       FREE(1);
       return i;
     }
