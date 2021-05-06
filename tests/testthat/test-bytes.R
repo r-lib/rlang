f3712805fec72f4bc9f7252d5075355dfeb1dc6b
Lionel Henry <lionel.hry@gmail.com>
2021-05-06 13:31:51 +0200

    Use modern style in bytes unit tests

--- tests/testthat/test-bytes.R
+++ tests/testthat/test-bytes.R
@@ -1,118 +1,114 @@
-describe("as_bench_bytes", {
-  it("accepts numeric input unchanged", {
-    expect_equal(unclass(as_bench_bytes(123L)), 123L)
-    expect_equal(unclass(as_bench_bytes(123)), 123)
-  })
-  it("accepts bench_byte input unchanged", {
-    x <- as_bench_bytes(123)
-    expect_equal(as_bench_bytes(x), x)
-  })
-  it("coerces character input", {
-    expect_equal(unclass(as_bench_bytes("1")), 1)
-    expect_equal(unclass(as_bench_bytes("1K")), 1024)
-    expect_equal(unclass(as_bench_bytes("1M")), 1024 * 1024)
-    expect_equal(unclass(as_bench_bytes("10M")), 10 * 1024 * 1024)
-    expect_equal(unclass(as_bench_bytes("1G")), 1024 * 1024 * 1024)
-  })
+test_that("bytes() coerces unspecified vectors but not logical ones", {
+  expect_equal(bytes2(c(NA, NA), NA), new_bytes(c(NA, NA, NA)))
+  expect_error(bytes2(TRUE), "Can't coerce a logical")
 })
 
-describe("format.bench_bytes", {
-  it("formats bytes under 1024 as whole numbers", {
-    expect_equal(format(bench_bytes(0)), "0B")
-    expect_equal(format(bench_bytes(1)), "1B")
-    expect_equal(format(bench_bytes(1023)), "1023B")
-  })
-  it("formats bytes 1024 and up as abbreviated numbers", {
-    expect_equal(format(bench_bytes(1024)), "1KB")
-    expect_equal(format(bench_bytes(1025)), "1KB")
-    expect_equal(format(bench_bytes(2^16)), "64KB")
-    expect_equal(format(bench_bytes(2^24)), "16MB")
-    expect_equal(format(bench_bytes(2^24 + 555555)), "16.5MB")
-    expect_equal(format(bench_bytes(2^32)), "4GB")
-    expect_equal(format(bench_bytes(2^48)), "256TB")
-    expect_equal(format(bench_bytes(2^64)), "16EB")
-  })
-  it("handles NA and NaN", {
-    expect_equal(format(bench_bytes(NA)), "NA")
-    expect_equal(format(bench_bytes(NaN)), "NaN")
-  })
-  it("works with vectors", {
-    v <- c(NA, 1, 2^13, 2^20, NaN, 2^15)
-    expect_equal(
-      format(bench_bytes(v), trim = TRUE),
-      c("NA", "1B", "8KB", "1MB", "NaN", "32KB"))
-
-    expect_equal(format(bench_bytes(numeric())), character())
-  })
+test_that("as_bytes() accepts numeric input unchanged", {
+  expect_equal(unclass(as_bytes(123L)), 123L)
+  expect_equal(unclass(as_bytes(123)), 123)
 })
 
-describe("sum.bench_bytes", {
-  it("sums its input and returns a bench_byte", {
-    expect_equal(sum(bench_bytes(0)), new_bench_bytes(0))
-    expect_equal(sum(bench_bytes(c(1, 2))), new_bench_bytes(3))
-    expect_equal(sum(bench_bytes(c(1, NA))), new_bench_bytes(NA_real_))
-  })
+test_that("as_bytes() accepts bench_byte input unchanged", {
+  x <- as_bytes(123)
+  expect_equal(as_bytes(x), x)
 })
 
-describe("min.bench_bytes", {
-  it("finds minimum input and returns a bench_byte", {
-    expect_equal(min(bench_bytes(0)), new_bench_bytes(0))
-    expect_equal(min(bench_bytes(c(1, 2))), new_bench_bytes(1))
-    expect_equal(min(bench_bytes(c(1, NA))), new_bench_bytes(NA_real_))
-  })
+test_that("parse_bytes() parses character input", {
+  expect_equal(unclass(parse_bytes("1")), 1)
+  expect_equal(unclass(parse_bytes("1K")), 1024)
+  expect_equal(unclass(parse_bytes("1M")), 1024 * 1024)
+  expect_equal(unclass(parse_bytes("10M")), 10 * 1024 * 1024)
+  expect_equal(unclass(parse_bytes("1G")), 1024 * 1024 * 1024)
 })
 
-describe("max.bench_bytes", {
-  it("finds maximum input and returns a bench_byte", {
-    expect_equal(max(bench_bytes(0)), new_bench_bytes(0))
-    expect_equal(max(bench_bytes(c(1, 2))), new_bench_bytes(2))
-    expect_equal(max(bench_bytes(c(1, NA))), new_bench_bytes(NA_real_))
-  })
+test_that("format.rlib_bytes() formats bytes under 1024 as whole numbers", {
+  expect_equal(format(bytes2(0)), "0B")
+  expect_equal(format(bytes2(1)), "1B")
+  expect_equal(format(bytes2(1023)), "1023B")
 })
 
-describe("[.bench_bytes", {
-  it("retains the bench_bytes class", {
-    x <- bench_bytes(c(100, 200, 300))
-    expect_equal(x[], x)
-    expect_equal(x[1], new_bench_bytes(100))
-    expect_equal(x[1:2], new_bench_bytes(c(100, 200)))
-  })
+test_that("format.rlib_bytes() formats bytes 1024 and up as abbreviated numbers", {
+  expect_equal(format(bytes2(1024)), "1KB")
+  expect_equal(format(bytes2(1025)), "1KB")
+  expect_equal(format(bytes2(2^16)), "64KB")
+  expect_equal(format(bytes2(2^24)), "16MB")
+  expect_equal(format(bytes2(2^24 + 555555)), "16.5MB")
+  expect_equal(format(bytes2(2^32)), "4GB")
+  expect_equal(format(bytes2(2^48)), "256TB")
+  expect_equal(format(bytes2(2^64)), "16EB")
 })
 
-describe("Ops.bench_bytes", {
-  it("errors for unary operators", {
-    x <- bench_bytes(c(100, 200, 300))
-    expect_error(!x, "unary '!' not defined for \"bench_bytes\" objects")
-    expect_error(+x, "unary '\\+' not defined for \"bench_bytes\" objects")
-    expect_error(-x, "unary '-' not defined for \"bench_bytes\" objects")
-  })
+test_that("format.rlib_bytes() handles NA and NaN", {
+  expect_equal(format(bytes2(NA)), "NA")
+  expect_equal(format(bytes2(NaN)), "NaN")
+})
 
-  it("works with boolean comparison operators", {
-    x <- bench_bytes(c(100, 200, 300))
+test_that("format.rlib_bytes() works with vectors", {
+  expect_equal(
+    format(as_bytes(c(NA, 1, 2^13, 2^20, NaN, 2^15)), trim = TRUE),
+    c("NA", "1B", "8KB", "1MB", "NaN", "32KB")
+  )
+  expect_equal(
+    format(as_bytes(numeric())),
+    character()
+  )
+})
 
-    expect_equal(x == 100, c(TRUE, FALSE, FALSE))
-    expect_equal(x != 100, c(FALSE, TRUE, TRUE))
-    expect_equal(x > 100, c(FALSE, TRUE, TRUE))
-    expect_equal(x >= 100, c(TRUE, TRUE, TRUE))
-    expect_equal(x < 200, c(TRUE, FALSE, FALSE))
-    expect_equal(x <= 200, c(TRUE, TRUE, FALSE))
-  })
+test_that("sum.rlib_bytes() sums its input and returns a bench_byte", {
+  expect_equal(sum(bytes2(0)), new_bytes(0))
+  expect_equal(sum(bytes2(c(1, 2))), new_bytes(3))
+  expect_equal(sum(bytes2(c(1, NA))), new_bytes(NA_real_))
+})
 
-  it("works with arithmetic operators", {
-    x <- bench_bytes(c(100, 200, 300))
+test_that("min.rlib_bytes() finds minimum input and returns a bench_byte", {
+  expect_equal(min(bytes2(0)), new_bytes(0))
+  expect_equal(min(bytes2(c(1, 2))), new_bytes(1))
+  expect_equal(min(bytes2(c(1, NA))), new_bytes(NA_real_))
+})
 
-    expect_equal(x + 100, bench_bytes(c(200, 300, 400)))
-    expect_equal(x - 100, bench_bytes(c(0, 100, 200)))
-    expect_equal(x * 100, bench_bytes(c(10000, 20000, 30000)))
-    expect_equal(x / 2, bench_bytes(c(50, 100, 150)))
-    expect_equal(x ^ 2, bench_bytes(c(10000, 40000, 90000)))
-  })
+test_that("max.rlib_bytes() finds maximum input and returns a bench_byte", {
+  expect_equal(max(bytes2(0)), new_bytes(0))
+  expect_equal(max(bytes2(c(1, 2))), new_bytes(2))
+  expect_equal(max(bytes2(c(1, NA))), new_bytes(NA_real_))
+})
 
-  it("errors for other binary operators", {
-    x <- bench_bytes(c(100, 200, 300))
-    expect_error(x %% 2, "'%%' not defined for \"bench_bytes\" objects")
-    expect_error(x %/% 2, "'%/%' not defined for \"bench_bytes\" objects")
-    expect_error(x & TRUE, "'&' not defined for \"bench_bytes\" objects")
-    expect_error(x | TRUE, "'|' not defined for \"bench_bytes\" objects")
-  })
+test_that("[.rlib_bytes() retains the bytes2 class", {
+  x <- bytes2(c(100, 200, 300))
+  expect_equal(x[], x)
+  expect_equal(x[1], new_bytes(100))
+  expect_equal(x[1:2], new_bytes(c(100, 200)))
+})
+
+test_that("Ops.rlib_bytes() errors for unary operators", {
+  x <- bytes2(c(100, 200, 300))
+  expect_error(!x, "unary `!` not defined for <rlib_bytes> objects")
+  expect_error(+x, "unary `\\+` not defined for <rlib_bytes> objects")
+  expect_error(-x, "unary `-` not defined for <rlib_bytes> objects")
+})
+
+test_that("Ops.rlib_bytes() works with boolean comparison operators", {
+  x <- bytes2(c(100, 200, 300))
+  expect_equal(x == 100, c(TRUE, FALSE, FALSE))
+  expect_equal(x != 100, c(FALSE, TRUE, TRUE))
+  expect_equal(x > 100, c(FALSE, TRUE, TRUE))
+  expect_equal(x >= 100, c(TRUE, TRUE, TRUE))
+  expect_equal(x < 200, c(TRUE, FALSE, FALSE))
+  expect_equal(x <= 200, c(TRUE, TRUE, FALSE))
+})
+
+test_that("Ops.rlib_bytes() works with arithmetic operators", {
+  x <- bytes2(c(100, 200, 300))
+  expect_equal(x + 100, bytes2(c(200, 300, 400)))
+  expect_equal(x - 100, bytes2(c(0, 100, 200)))
+  expect_equal(x * 100, bytes2(c(10000, 20000, 30000)))
+  expect_equal(x / 2, bytes2(c(50, 100, 150)))
+  expect_equal(x ^ 2, bytes2(c(10000, 40000, 90000)))
+})
+
+test_that("Ops.rlib_bytes() errors for other binary operators", {
+  x <- bytes2(c(100, 200, 300))
+  expect_error(x %% 2, "`%%` not defined for <rlib_bytes> objects")
+  expect_error(x %/% 2, "`%/%` not defined for <rlib_bytes> objects")
+  expect_error(x & TRUE, "`&` not defined for <rlib_bytes> objects")
+  expect_error(x | TRUE, "`|` not defined for <rlib_bytes> objects")
 })
