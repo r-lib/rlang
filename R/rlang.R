664abc30ec00fe269876517a27267cf26ca280a3
Lionel Henry <lionel.hry@gmail.com>
2021-05-07 17:00:32 +0200

    Require ellipsis 0.3.2

--- R/rlang.R
+++ R/rlang.R
@@ -2,10 +2,8 @@
 "_PACKAGE"
 
 on_load({
-  check_downstream(
-    "0.4.0",
-    "dplyr (>= 0.8.0)"
-  )
+  check_downstream("0.4.0", "dplyr (>= 0.8.0)")
+  check_downstream("0.5.0", "ellipsis (>= 0.3.2)")
 })
 
 .rlang_use_cli_format <- "try"
