13bf8d03bc15b9ee613aad37d3d67193e7eb2ffa
Kirill Müller <krlmlr+r@mailbox.org>
2021-05-21 04:58:49 +0200

    Overwrite deprecated with lifecycle::deprecated

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,16 @@
   .Call(ffi_init_rlang, rlang_ns)
 
   run_on_load()
+
+  # Avoid warnings from clash between rlang::deprecated and lifecycle::deprecated
+  on_package_load("lifecycle", {
+    env <- get_env(deprecated)
+    old <- env_binding_unlock(env, "deprecated")
+    assign("deprecated", lifecycle::deprecated, env)
+    if (old) {
+      env_binding_lock(env, "deprecated")
+    }
+  })
 }
 .onUnload <- function(lib) {
   .Call(ffi_fini_rlang)
