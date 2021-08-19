be6a40e8b0afeb829bd1a977d16950f4886a1522
Davis Vaughan <davis@rstudio.com>
2021-08-19 09:30:28 -0400

    Allow `names_inform_repair()` to be silenced by a global option (#1249)
    * Allow `names_inform_repair()` to be silenced by a global option
    
    This also adds the `"rlib_message_name_repair"` class to the message signaled by `names_inform_repair()`, giving us yet another method to handle it.
    
    * Update snapshot tests

--- R/names.R
+++ R/names.R
@@ -3,7 +3,24 @@ names_as_unique <- function(names, ..., quiet = FALSE) {
   .Call(ffi_names_as_unique, names, quiet)
 }
 
-#' Inform about names repair
+#' Inform about name repair
+#'
+#' @section Muffling and silencing messages:
+#'
+#' Name repair messages are signaled with [inform()] and are given the class
+#' `"rlib_message_name_repair"`. These messages can be muffled with
+#' [base::suppressMessages()].
+#'
+#' Name repair messages can also be silenced with the global option
+#' `rlib_name_repair_verbosity`. This option takes the values:
+#'
+#' - `"verbose"`: Always verbose.
+#' - `"quiet"`: Always quiet.
+#'
+#' When set to quiet, the message is not displayed and the condition is not
+#' signaled. This is particularly useful for silencing messages during testing
+#' when combined with [local_options()].
+#'
 #' @param old Original names vector.
 #' @param new Repaired names vector.
 #' @keywords internal
@@ -18,20 +35,37 @@ names_inform_repair <- function(old, new) {
     length(old) == length(new)
   )
 
+  if (peek_name_repair_verbosity() == "quiet") {
+    return(invisible())
+  }
+
   old <- old %|% ""
   new_names <- new != old
-  if (any(new_names)) {
-    bullets <- paste0(
-      tick_if_needed(old[new_names]),
-      " -> ",
-      tick_if_needed(new[new_names]),
-      .problem = ""
-    )
-    inform(c(
-      "New names:",
-      set_names(bullets, "*"))
-    )
+
+  if (!any(new_names)) {
+    return(invisible())
   }
+
+  bullets <- paste0(
+    tick_if_needed(old[new_names]),
+    " -> ",
+    tick_if_needed(new[new_names]),
+    .problem = ""
+  )
+
+  message <- c(
+    "New names:",
+    set_names(bullets, "*")
+  )
+
+  inform(message = message, class = "rlib_message_name_repair")
+}
+
+peek_name_repair_verbosity <- function() {
+  opt <- "rlib_name_repair_verbosity"
+  out <- peek_option(opt) %||% "verbose"
+  out <- arg_match0(out, c("verbose", "quiet"), opt)
+  out
 }
 
 tick_if_needed <- function(x) {
