26c4a878473578cf7b4c630f457a3bdfc46c8c31
Lionel Henry <lionel.hry@gmail.com>
2021-10-05 14:38:56 +0200

    Deprecate `expr_interp()`

--- R/lifecycle-retired.R
+++ R/lifecycle-retired.R
@@ -1199,6 +1199,59 @@ overscope_eval_next <- function(overscope, quo, env = base_env()) {
   .External2(ffi_eval_tidy, quo, overscope, env)
 }
 
+#' Process unquote operators in a captured expression
+#'
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
+#' `expr_interp()` is deprecated, please use [inject()] instead.
+#'
+#' @param x A function, raw expression, or formula to interpolate.
+#' @param env The environment in which unquoted expressions should be
+#'   evaluated. By default, the formula or closure environment if a
+#'   formula or a function, or the current environment otherwise.
+#' @examples
+#' # All tidy NSE functions like quo() unquote on capture:
+#' quo(list(!!(1 + 2)))
+#'
+#' # expr_interp() is meant to provide the same functionality when you
+#' # have a formula or expression that might contain unquoting
+#' # operators:
+#' f <- ~list(!!(1 + 2))
+#' expr_interp(f)
+#'
+#' # Note that only the outer formula is unquoted (which is a reason
+#' # to use expr_interp() as early as possible in all user-facing
+#' # functions):
+#' f <- ~list(~!!(1 + 2), !!(1 + 2))
+#' expr_interp(f)
+#'
+#'
+#' # Another purpose for expr_interp() is to interpolate a closure's
+#' # body. This is useful to inline a function within another. The
+#' # important limitation is that all formal arguments of the inlined
+#' # function should be defined in the receiving function:
+#' other_fn <- function(x) toupper(x)
+#'
+#' fn <- expr_interp(function(x) {
+#'   x <- paste0(x, "_suffix")
+#'   !!! body(other_fn)
+#' })
+#' fn
+#' fn("foo")
+#' @keywords internal
+#' @export
+expr_interp <- function(x, env = NULL) {
+  if (is_formula(x)) {
+    f_rhs(x) <- .Call(ffi_interp, f_rhs(x), env %||% f_env(x))
+  } else if (is_closure(x)) {
+    body(x) <- .Call(ffi_interp, body(x), env %||% fn_env(x))
+  } else {
+    x <- .Call(ffi_interp, x, env %||% parent.frame())
+  }
+  x
+}
+
 
 #  Expressions  ------------------------------------------------------
 
--- R/nse-force.R
+++ R/nse-force.R
@@ -266,60 +266,6 @@ qq_show <- function(expr) {
   expr_print(enexpr(expr))
 }
 
-#' Process unquote operators in a captured expression
-#'
-#' While all capturing functions in the tidy evaluation framework
-#' perform unquote on capture (most notably [quo()]),
-#' `expr_interp()` manually processes unquoting operators in
-#' expressions that are already captured. `expr_interp()` should be
-#' called in all user-facing functions expecting a formula as argument
-#' to provide the same quasiquotation functionality as NSE functions.
-#'
-#' @param x A function, raw expression, or formula to interpolate.
-#' @param env The environment in which unquoted expressions should be
-#'   evaluated. By default, the formula or closure environment if a
-#'   formula or a function, or the current environment otherwise.
-#' @export
-#' @examples
-#' # All tidy NSE functions like quo() unquote on capture:
-#' quo(list(!!(1 + 2)))
-#'
-#' # expr_interp() is meant to provide the same functionality when you
-#' # have a formula or expression that might contain unquoting
-#' # operators:
-#' f <- ~list(!!(1 + 2))
-#' expr_interp(f)
-#'
-#' # Note that only the outer formula is unquoted (which is a reason
-#' # to use expr_interp() as early as possible in all user-facing
-#' # functions):
-#' f <- ~list(~!!(1 + 2), !!(1 + 2))
-#' expr_interp(f)
-#'
-#'
-#' # Another purpose for expr_interp() is to interpolate a closure's
-#' # body. This is useful to inline a function within another. The
-#' # important limitation is that all formal arguments of the inlined
-#' # function should be defined in the receiving function:
-#' other_fn <- function(x) toupper(x)
-#'
-#' fn <- expr_interp(function(x) {
-#'   x <- paste0(x, "_suffix")
-#'   !!! body(other_fn)
-#' })
-#' fn
-#' fn("foo")
-expr_interp <- function(x, env = NULL) {
-  if (is_formula(x)) {
-    f_rhs(x) <- .Call(ffi_interp, f_rhs(x), env %||% f_env(x))
-  } else if (is_closure(x)) {
-    body(x) <- .Call(ffi_interp, body(x), env %||% fn_env(x))
-  } else {
-    x <- .Call(ffi_interp, x, env %||% parent.frame())
-  }
-  x
-}
-
 
 glue_unquote <- function(text, env = caller_env()) {
   glue::glue(glue_first_pass(text, env = env), .envir = env)
