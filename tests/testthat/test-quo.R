287bc411beca7d5a070b6577665da2446f6dcd42
Lionel Henry <lionel.hry@gmail.com>
2021-10-18 08:57:06 +0200

    Fix `expect_warning()` test

--- tests/testthat/test-quo.R
+++ tests/testthat/test-quo.R
@@ -284,7 +284,7 @@ test_that("quosure lists are considered vectors", {
 
 test_that("as_quosure() still provides default env", {
   local_lifecycle_warnings()
-  quo <- expect_warning(as_quosure(quote(foo)), "explicit environment")
+  expect_warning(quo <- as_quosure(quote(foo)), "explicit environment")
   expect_reference(quo_get_env(quo), current_env())
 })
 
