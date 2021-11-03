ceb97e4b0e2059e2b575bbea0cdfeda1caee9226
Lionel Henry <lionel.hry@gmail.com>
2021-11-03 15:38:07 +0100

    Review `last_error()` and `last_warnings()` topics

--- R/cnd-last.R
+++ R/cnd-last.R
@@ -1,3 +1,68 @@
+#' Last `abort()` error
+#'
+#' @description
+#' * `last_error()` returns the last error entraced by [abort()] or
+#'   [global_entrace()]. The error is printed with a backtrace in
+#'   simplified form.
+#'
+#' * `last_trace()` is a shortcut to return the backtrace stored in
+#'   the last error. This backtrace is printed in full form.
+#'
+#' @seealso
+#' * [`rlang_backtrace_on_error`] to control what is displayed when an
+#'   error is thrown.
+#'
+#' * [global_entrace()] to enable `last_error()` logging for all errors.
+#'
+#' * [last_warnings()] and [last_messages()].
+#'
+#' @export
+last_error <- function() {
+  err <- peek_last_error()
+
+  if (is_null(err)) {
+    local_options(rlang_backtrace_on_error = "none")
+    stop("Can't show last error because no error was recorded yet", call. = FALSE)
+  }
+
+  err$rlang$internal$from_last_error <- TRUE
+  err
+}
+#' @rdname last_error
+#' @export
+last_trace <- function() {
+  err <- last_error()
+  err$rlang$internal$print_simplify <- "none"
+  err
+}
+
+peek_last_error <- function(cnd) {
+  last_error_env()$.Last.error
+}
+poke_last_error <- function(cnd) {
+  env <- last_error_env()
+
+  env$.Last.error <- cnd
+  env$.Last.error.trace <- cnd$trace
+
+  invisible(cnd)
+}
+last_error_env <- function() {
+  if (!is_attached("org:r-lib")) {
+    exec(
+      attach,
+      list(
+        .Last.error = NULL,
+        .Last.error.trace = NULL
+      ),
+      pos = length(search()),
+      name = "org:r-lib"
+    )
+  }
+
+  search_env("org:r-lib")
+}
+
 #' Display last warnings
 #'
 #' @description
@@ -14,21 +79,88 @@
 #'
 #' @param n How many warnings or messages to display. Defaults to all.
 #'
-#' @examples
-#' if (FALSE) {
-#'   global_entrace()
-#'   f <- function() { warning("foo"); g() }
-#'   g <- function() { warning("bar", immediate. = TRUE); h() }
-#'   h <- function() warning("baz")
+#' @seealso [last_error()]
 #'
-#'   f()
+#' @section Examples:
 #'
-#'   # Simplified backtraces
-#'   last_warnings()
+#' Enable backtrace capture with `global_entrace()`:
 #'
-#'   # Full backtraces
-#'   summary(last_warnings())
-#' }
+#' ```r
+#' global_entrace()
+#' ```
+#'
+#' Signal some warnings in nested functions. The warnings inform about
+#' which function emitted a warning but they don't provide information
+#' about the call stack:
+#'
+#' ```r
+#' f <- function() { warning("foo"); g() }
+#' g <- function() { warning("bar", immediate. = TRUE); h() }
+#' h <- function() warning("baz")
+#'
+#' f()
+#' #> Warning in g() : bar
+#' #> Warning messages:
+#' #> 1: In f() : foo
+#' #> 2: In h() : baz
+#' ```
+#'
+#' Call `last_warnings()` to see backtraces for each of these warnings:
+#'
+#' ```r
+#' last_warnings()
+#' #> [[1]]
+#' #> <warning/rlang_warning>
+#' #> Warning in `f()`: foo
+#' #> Backtrace:
+#' #>  1. global f()
+#' #>
+#' #> [[2]]
+#' #> <warning/rlang_warning>
+#' #> Warning in `g()`: bar
+#' #> Backtrace:
+#' #>  1. global f()
+#' #>  2. global g()
+#' #>
+#' #> [[3]]
+#' #> <warning/rlang_warning>
+#' #> Warning in `h()`: baz
+#' #> Backtrace:
+#' #>  1. global f()
+#' #>  2. global g()
+#' #>  3. global h()
+#' ```
+#'
+#' To get a full backtrace, use [summary()]. In this case the full
+#' backtraces do not include more information:
+#'
+#' ```r
+#' summary(last_warnings())
+#' #> [[1]]
+#' #> <warning/rlang_warning>
+#' #> Warning in `f()`: foo
+#' #> Backtrace:
+#' #>     ▆
+#' #>  1. └─global f()
+#' #>
+#' #> [[2]]
+#' #> <warning/rlang_warning>
+#' #> Warning in `g()`: bar
+#' #> Backtrace:
+#' #>     ▆
+#' #>  1. └─global f()
+#' #>  2.   └─global g()
+#' #>
+#' #> [[3]]
+#' #> <warning/rlang_warning>
+#' #> Warning in `h()`: baz
+#' #> Backtrace:
+#' #>     ▆
+#' #>  1. └─global f()
+#' #>  2.   └─global g()
+#' #>  3.     └─global h()
+#' ```
+#'
 #' @export
 last_warnings <- function(n = NULL) {
   out <- new_list_of_conditions(the$last_warnings)
