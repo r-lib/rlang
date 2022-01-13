0f6d3f1d38757ff187d561983d20708e055908c8
Lionel Henry <lionel.hry@gmail.com>
2022-01-13 15:56:32 +0100

    Move `with_handlers()` test to deprecated test file

--- tests/testthat/test-retired.R
+++ tests/testthat/test-retired.R
@@ -123,3 +123,46 @@ test_that("call_fn() looks up function in `env`", {
   })
   expect_identical(call_fn(quote(fn()), env = env), env$fn)
 })
+
+test_that("with_handlers() establishes inplace and exiting handlers", {
+  handlers <- list(
+    error = function(c) "caught error",
+    message = function(c) "caught message",
+    warning = calling(function(c) "warning"),
+    foobar = calling(function(c) cat("foobar"))
+  )
+
+  expect_equal(with_handlers(identity(letters), !!!handlers), identity(letters))
+  expect_equal(with_handlers(stop(letters), !!!handlers), "caught error")
+  expect_equal(with_handlers(message(letters), !!!handlers), "caught message")
+  expect_warning(expect_equal(with_handlers({ warning("warn!"); letters }, !!!handlers), identity(letters)), "warn!")
+  expect_output(expect_equal(with_handlers({ signal("", "foobar"); letters }, !!!handlers), identity(letters)), "foobar")
+})
+
+test_that("with_handlers() propagates visibility", {
+  expect_visible(with_handlers(list(invisible(1))))
+  expect_invisible(with_handlers(invisible(1)))
+})
+
+test_that("bare functions are treated as exiting handlers", {
+  expect_identical(with_handlers(abort("foo"), error = function(cnd) "caught"), "caught")
+})
+
+test_that("with_handlers() supports formula shortcut for lambdas", {
+  err <- with_handlers(abort("foo", "bar"), error = ~.x)
+  expect_true(inherits(err, "bar"))
+})
+
+test_that("can muffle conditions", {
+  expect_no_message(
+    expect_identical(with_handlers({ message(""); "foo" }, message = calling(cnd_muffle)), "foo")
+  )
+  expect_no_warning(
+    expect_identical(with_handlers({ warning(""); "foo" }, warning = calling(cnd_muffle)), "foo")
+  )
+  cnd_expect_muffle <- calling(function(cnd) {
+    expect_s3_class(findRestart("rlang_muffle"), "restart")
+    cnd_muffle(cnd)
+  })
+  expect_identical(with_handlers({ signal("", "cnd"); "foo" }, cnd = cnd_expect_muffle), "foo")
+})
