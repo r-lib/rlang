e591324473973669a3fc876131b3a42d210e4239
Lionel Henry <lionel.hry@gmail.com>
2022-01-19 10:22:23 +0100

    Update package version in `check_downstream()`

--- R/rlang.R
+++ R/rlang.R
@@ -5,7 +5,7 @@
 on_load({
   check_downstream("0.4.0", "dplyr (>= 0.8.0)")
   check_downstream(
-    "0.5.0",
+    "1.0.0",
     "ellipsis (>= 0.3.2)",
     "vctrs (>= 0.3.8)",
     info = "Not updating now is completely safe and will only cause import warnings."
