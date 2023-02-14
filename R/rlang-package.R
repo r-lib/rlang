511a898a7e3165b531c55ece5555af4be9ac30fe
Lionel Henry <lionel.hry@gmail.com>
2023-02-14 15:20:19 +0100

    Add doc page for exported FFI symbols

--- R/rlang-package.R
+++ R/rlang-package.R
@@ -10,5 +10,8 @@ compiled_by_gcc <- function() {
   .Call(ffi_compiled_by_gcc)
 }
 
+#' Internal API for standalone-types-check
+#' @name ffi_standalone_types_check
+#' @keywords internal
 #' @rawNamespace export(ffi_standalone_check_is_bool)
 NULL
