fe90cacc954832a688618b10239833744aa55068
Lionel Henry <lionel.hry@gmail.com>
2023-02-14 16:13:17 +0100

    Fix name

--- R/rlang-package.R
+++ R/rlang-package.R
@@ -12,7 +12,7 @@ compiled_by_gcc <- function() {
 
 #' Internal API for standalone-types-check
 #' @name ffi_standalone_types_check
-#' @aliases ffi_standalone_check_bool_1.0.7
+#' @aliases ffi_standalone_is_bool_1.0.7
 #' @keywords internal
-#' @rawNamespace export(ffi_standalone_check_bool_1.0.7)
+#' @rawNamespace export(ffi_standalone_is_bool_1.0.7)
 NULL
