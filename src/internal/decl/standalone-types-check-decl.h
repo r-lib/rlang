16464c415ec58096129911b313048fee2f35b869
Lionel Henry <lionel.hry@gmail.com>
2023-02-15 16:59:38 +0100

    Match names in declaration
    Co-authored-by: Davis Vaughan <davis@rstudio.com>

--- src/internal/decl/standalone-types-check-decl.h
+++ src/internal/decl/standalone-types-check-decl.h
@@ -8,8 +8,8 @@ enum is_number int_standalone_check_number(r_obj* x,
 static
 enum is_number dbl_standalone_check_number(r_obj* x,
                                            r_obj* allow_decimal,
-                                           r_obj* min,
-                                           r_obj* max,
+                                           r_obj* ffi_min,
+                                           r_obj* ffi_max,
                                            r_obj* allow_infinite,
                                            r_obj* allow_na,
                                            r_obj* allow_null);
