a82272e30f75920b55a85fd55e010ecc0197436d
Lionel Henry <lionel.hry@gmail.com>
2023-03-13 10:38:22 +0100

    Increment version number to 1.1.0

--- src/version.c
+++ src/version.c
@@ -1,7 +1,7 @@
 #define R_NO_REMAP
 #include <Rinternals.h>
 
-const char* rlang_version = "1.0.6.9000";
+const char* rlang_version = "1.1.0";
 
 /**
  * This file records the expected package version in the shared
