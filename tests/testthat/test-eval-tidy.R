7841b3ed642b2a6590f3fc8a9f43878132003507
Lionel Henry <lionel.hry@gmail.com>
2024-05-31 09:56:06 +0200

    Skip quosure infloop test on CRAN

--- tests/testthat/test-eval-tidy.R
+++ tests/testthat/test-eval-tidy.R
@@ -454,6 +454,9 @@ test_that("can't take the names() and length() of the `.data` pronoun", {
 })
 
 test_that("eval_tidy() does not infloop when the quosure inherits from the mask", {
+  # New r-devel error: cycles in parent chains are not allowed
+  skip_on_cran()
+
   mask <- as_data_mask(list(foo = 1))
   quo <- new_quosure(quote(foo), mask)
   expect_identical(eval_tidy(quo, mask), 1)
