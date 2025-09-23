3057c42d0475395e454ef35687115b31dea80ebb
Jon Harmon <jonthegeek@gmail.com>
2025-09-23 06:28:02 -0500

    NA + 1i is not syntactic literal (#1829)
    Fixes #1828.

--- tests/testthat/test-expr.R
+++ tests/testthat/test-expr.R
@@ -145,3 +145,9 @@ test_that("negative numbers are not syntactic", {
   expect_false(is_syntactic_literal(-1L))
   expect_false(is_syntactic_literal(-1i))
 })
+
+test_that("NA + 1i is not syntactic", {
+  skip_if_not_installed("base", "4.4")
+  expect_false(is_syntactic_literal(NA + 1i))
+  expect_false(is_syntactic_literal(NA - 1i))
+})
