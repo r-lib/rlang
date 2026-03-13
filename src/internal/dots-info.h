794d66054866dba0cd65701f9340045e440a2208
Lionel Henry <lionel.hry@proton.me>
2026-03-13 18:15:27 +0100

    Remove ffi declarations from header

--- src/internal/dots-info.h
+++ src/internal/dots-info.h
@@ -3,7 +3,7 @@
 
 #include <rlang.h>
 
-// Dots API - mirrors R-devel PR #209 ---
+// Dots API - mirrors R-devel PR #209
 
 typedef enum {
     DOT_TYPE_value = 0,
@@ -15,24 +15,11 @@ typedef enum {
 bool r_env_dots_exist(r_obj* env);
 r_ssize r_env_dots_length(r_obj* env);
 r_obj* r_env_dots_names(r_obj* env);
-r_obj* r_env_dot_get(r_obj* env, r_ssize i);
 
 r_dot_type_t r_env_dot_type(r_obj* env, r_ssize i);
-
+r_obj* r_env_dot_get(r_obj* env, r_ssize i);
 r_obj* r_env_dot_delayed_expr(r_obj* env, r_ssize i);
 r_obj* r_env_dot_delayed_env(r_obj* env, r_ssize i);
 r_obj* r_env_dot_forced_expr(r_obj* env, r_ssize i);
-
-
-// FFI wrappers for R interface ---
-
-r_obj* ffi_dots_exist(r_obj* env);
-r_obj* ffi_dots_length(r_obj* env);
-r_obj* ffi_dots_names(r_obj* env);
-r_obj* ffi_dot_get(r_obj* ffi_i, r_obj* env);
-r_obj* ffi_dot_type(r_obj* ffi_i, r_obj* env);
-r_obj* ffi_dot_delayed_expr(r_obj* ffi_i, r_obj* env);
-r_obj* ffi_dot_delayed_env(r_obj* ffi_i, r_obj* env);
-r_obj* ffi_dot_forced_expr(r_obj* ffi_i, r_obj* env);
 
 #endif /* RLANG_DOTS_API_H */
